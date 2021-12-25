7df9362a9a5a7903766a19555c056d2c9497f924
carlomazzaferro <carlo.mazzaferro@gmail.com>
2021-12-25 12:36:13 -0300

    Price oracle deployment spec

---
+++ test/priceOracle.spec.ts
@@ -0,0 +1,21 @@
+import { deployments, ethers, network } from "hardhat";
+import { getContractsConfig } from "./utilities";
+import { utils } from "ethers";
+import { expect } from "chai";
+import { Fuse } from "../lib/esm";
+
+describe("PriceOracle deployment", function () {
+  beforeEach(async function () {
+    await deployments.fixture(); // ensure you start from a fresh deployments
+  });
+  describe("Deploy ChainLinkPriceOracle", async function () {
+    it.only("should deploy the price oracle via sdk", async function () {
+      const { alice } = await ethers.getNamedSigners();
+      const contractConfig = await getContractsConfig(network.name);
+      const sdk = new Fuse(ethers.provider, contractConfig);
+
+      const deployedOracle = await sdk.deployPriceOracle("ChainlinkPriceOracle", {}, { from: alice.address });
+      expect(deployedOracle).to.be.ok;
+    });
+  });
+});
