0c264346294342c1e861f9842f1c86772c079954
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-01-27 08:43:08 +0100

    Do not recreate poool if it exists

--- tasks/createPoolsWithAssets.ts
+++ tasks/createPoolsWithAssets.ts
@@ -1,4 +1,5 @@
 import { task, types } from "hardhat/config";
+import { getPoolByName } from "../test/utils/pool";
 
 const logPoolData = async (poolAddress, creatorAddress, sdk) => {
   const poolModule = await import("../test/utils/pool");
@@ -6,14 +7,14 @@ const logPoolData = async (poolAddress, creatorAddress, sdk) => {
   const fusePoolData = await sdk.fetchFusePoolData(poolIndex, poolAddress);
 
   const poolAssets = fusePoolData.assets.map((a) => a.underlyingSymbol).join(", ");
-  console.log(`Pool with address ${poolAddress},  mame: ${fusePoolData.name}, assets ${poolAssets} created!`);
+  console.log(`Operating on pool with address ${poolAddress},  mame: ${fusePoolData.name}, assets ${poolAssets}`);
 };
 
 export default task("create-pools", "Create Testing Pools")
   .addParam("name", "Name of the pool to be created")
-  .addOptionalParam("depositAmount", "Amount to deposit", 0, types.int)
+  .addOptionalParam("depositAmount", "Amount to deposit", 0, types.string)
   .addOptionalParam("depositSymbol", "Amount to deposit", "ETH")
-  .addOptionalParam("borrowAmount", "Amount to deposit", 0, types.int)
+  .addOptionalParam("borrowAmount", "Amount to deposit", 0, types.string)
   .addOptionalParam("borrowSymbol", "Amount to deposit", "ETH")
   .setAction(
     async (
@@ -30,12 +31,21 @@ export default task("create-pools", "Create Testing Pools")
       // @ts-ignore
       const sdkModule = await import("../lib/esm/src");
       const collateralModule = await import("../test/utils/collateral");
-
       const sdk = new sdkModule.Fuse(ethers.provider, "1337");
       const { bob } = await ethers.getNamedSigners();
-      const [poolAddress] = await poolModule.createPool({ ethers, poolName: _name, signer: bob });
-      const assets = await poolModule.getAssetsConf(ethers, poolAddress);
-      await poolModule.deployAssets(ethers, assets.assets, bob);
+
+      const existingPool = await getPoolByName(_name, bob.address, sdk);
+
+      let poolAddress: string;
+      if (existingPool !== null) {
+        console.log(`Pool with name ${existingPool.name} exists already, will operate on it`);
+        poolAddress = existingPool.comptroller;
+      } else {
+        [poolAddress] = await poolModule.createPool({ ethers, poolName: _name, signer: bob });
+        const assets = await poolModule.getAssetsConf(ethers, poolAddress);
+        await poolModule.deployAssets(ethers, assets.assets, bob);
+      }
+
       await logPoolData(poolAddress, bob.address, sdk);
       if (_depositAmount != 0) {
         await collateralModule.addCollateral(
