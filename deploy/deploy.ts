efa679c62f17c66ee02efd5eee127d13ac165878
RedVeil <leon.niesler@gmail.com>
2022-02-04 21:00:42 +0100

    addded CErc20PluginDelegate into the deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -243,7 +243,17 @@ const func: DeployFunction = async ({ ethers, getNamedAccounts, deployments, get
   } else if (chainId === "97") {
     await deploy97({ ethers, getNamedAccounts, deployments });
   }
+
   ////
+  //// Deploy CErc20PluginDelegate
+  dep = await deployments.deterministic("CErc20PluginDelegate", {
+    from: deployer,
+    salt: ethers.utils.keccak256(ethers.utils.toUtf8Bytes(SALT)),
+    args: [],
+    log: true,
+  });
+  const pluginDelegate = await dep.deploy();
+  console.log("pluginDelegate:", pluginDelegate.address);
 };
 
 export default func;
