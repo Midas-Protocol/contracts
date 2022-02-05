59c6cb6119f803d7b6cd35e7a379ae58bb4b986b
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-02-05 17:34:13 +0100

    Remove unused values, add fee consts

--- test/utils/config.ts
+++ test/utils/config.ts
@@ -1,16 +1,3 @@
-import { ethers } from "hardhat";
-import * as fs from "fs";
-
-export async function readFile<T>(file: string, fn: (data: string) => T): Promise<T> {
-  return new Promise((resolve, reject) => {
-    fs.access(file, fs.constants.F_OK, (err) => {
-      if (err) {
-        console.log(`Error reading file ${err}`);
-      } else {
-        fs.readFile(file, "utf8", (err, data) => {
-          return err ? reject(err) : resolve(fn(data));
-        });
-      }
-    });
-  });
-}
+// user per thousand instead of per cent to avoid BN issues
+export const FUSE_LIQUIDATION_SEIZE_FEE_PER_THOUSAND = 100;
+export const FUSE_LIQUIDATION_PROTOCOL_FEE_PER_THOUSAND = 28;
