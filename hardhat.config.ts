d7c2e91d3c9206d2e74fc55268022bc90628b545
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-02-07 15:02:13 +0200

    bump openzeppelin version to 4.4.2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -32,6 +32,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: "0.7.6",
         settings: {
           optimizer: {
