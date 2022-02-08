ab45965caaf1cedbbcabf93d72c1cb643e02dd93
Rahul Sethuram <rahul@sethuram.com>
2022-02-08 13:40:16 +0100

    Fix test

--- contracts_forge/BeefyERC4626.t.sol
+++ contracts_forge/BeefyERC4626.t.sol
@@ -5,8 +5,9 @@ import "forge-std/stdlib.sol";
 import "forge-std/Vm.sol";
 
 import "../contracts/compound/strategies/BeefyERC4626.sol";
+import {ERC20} from "@rari-capital/solmate/src/tokens/ERC20.sol";
 
-contract BeefyERC4626 is DSTest {
+contract BeefyERC4626Test is DSTest {
   using stdStorage for StdStorage;
 
   Vm public constant vm = Vm(HEVM_ADDRESS);
@@ -16,7 +17,12 @@ contract BeefyERC4626 is DSTest {
   BeefyERC4626 beefyERC4626;
 
   function setUp() public {
-    beefyERC4626 = new BeefyERC4626(0x000, "test", "test", 0x000);
+    beefyERC4626 = new BeefyERC4626(
+      ERC20(0x0000000000000000000000000000000000000000),
+      "test",
+      "test",
+      IBeefyVault(0x0000000000000000000000000000000000000000)
+    );
   }
 
   function testName() public {
