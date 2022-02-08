52e0516bad717f173df2deb1ccb39b4e2666c7e8
Leon Niesler <leon.niesler@gmail.com>
2022-02-08 11:16:31 +0100

    wip all contracts compile

--- contracts/compound/strategies/AutofarmERC4626.sol
+++ contracts/compound/strategies/AutofarmERC4626.sol
@@ -5,27 +5,27 @@ import {ERC20} from "@rari-capital/solmate/src/tokens/ERC20.sol";
 import {ERC4626} from "../../utils/ERC4626.sol";
 import {SafeTransferLib} from "@rari-capital/solmate/src/utils/SafeTransferLib.sol";
 
-interface IBeefyVault {
-  function deposit(uint256 _amount) external;
+interface IAutofarmStratX2 {
+  function deposit(address _userAddress, uint256 _wantAmt) external returns (uint256);
 
-  function withdraw(uint256 _shares) external;
+  function withdraw(address _userAddress, uint256 _wantAmt) external returns (uint256);
 
   function balanceOf(address _account) external view returns (uint256);
 }
 
 /**
- * @title Beefy ERC4626 Contract
- * @notice ERC4626 wrapper for beefy vaults
+ * @title Autofarm ERC4626 Contract
+ * @notice ERC4626 wrapper for Autofarm StratX2
  * @author RedVeil
  *
- * Wraps https://github.com/beefyfinance/beefy-contracts/blob/master/contracts/BIFI/vaults/BeefyVaultV6.sol
+ * Wraps https://github.com/autofarmnetwork/AutofarmV2_CrossChain/blob/master/StratX2.sol
  */
-contract BeefyERC4626 is ERC4626 {
+contract AutofarmERC4626 is ERC4626 {
   using SafeTransferLib for ERC20;
 
   /* ========== STATE VARIABLES ========== */
 
-  IBeefyVault public immutable beefyVault;
+  IAutofarmStratX2 public immutable autofarmStrat;
 
   /* ========== CONSTRUCTOR ========== */
 
@@ -34,15 +34,15 @@ contract BeefyERC4626 is ERC4626 {
      @param _asset The ERC20 compliant token the Vault should accept.
      @param _name The name for the vault token.
      @param _symbol The symbol for the vault token.
-     @param _beefyVault The Beefy Vault contract.
+     @param _autofarmStrat The Beefy Vault contract.
     */
   constructor(
     ERC20 _asset,
     string memory _name,
     string memory _symbol,
-    IBeefyVault _beefyVault
+    IAutofarmStratX2 _autofarmStrat
   ) ERC4626(_asset, _name, _symbol) {
-    beefyVault = _beefyVault;
+    autofarmStrat = _autofarmStrat;
   }
 
   /* ========== VIEWS ========== */
@@ -50,24 +50,17 @@ contract BeefyERC4626 is ERC4626 {
   /// @notice Calculates the total amount of underlying tokens the Vault holds.
   /// @return The total amount of underlying tokens the Vault holds.
   function totalAssets() public view override returns (uint256) {
-    return beefyVault.balanceOf(address(this));
-  }
-
-  /// @notice Calculates the total amount of underlying tokens the Vault holds.
-  /// @return The total amount of underlying tokens the Vault holds.
-  function balanceOfUnderlying(address _x) public view returns (uint256) {
-    _x;
-    return beefyVault.balanceOf(address(this));
+    return autofarmStrat.balanceOf(address(this));
   }
 
   /* ========== INTERNAL FUNCTIONS ========== */
 
   function afterDeposit(uint256 amount, uint256 shares) internal override {
-    asset.approve(address(beefyVault), amount);
-    beefyVault.deposit(amount);
+    asset.approve(address(autofarmStrat), amount);
+    autofarmStrat.deposit(address(this), amount);
   }
 
   function beforeWithdraw(uint256 amount, uint256 shares) internal override {
-    beefyVault.withdraw(shares);
+    autofarmStrat.withdraw(address(this), shares);
   }
 }
