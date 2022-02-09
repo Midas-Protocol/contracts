d1a68676d594892e1a29c38d0ab9a0c6fe4a3ceb
RedVeil <leon.niesler@gmail.com>
2022-02-09 09:59:34 +0100

    updated beefy,autofarm erc4626 + added alpaca erc4626

--- contracts/compound/strategies/AutofarmERC4626.sol
+++ contracts/compound/strategies/AutofarmERC4626.sol
@@ -5,27 +5,29 @@ import {ERC20} from "@rari-capital/solmate/src/tokens/ERC20.sol";
 import {ERC4626} from "../../utils/ERC4626.sol";
 import {SafeTransferLib} from "@rari-capital/solmate/src/utils/SafeTransferLib.sol";
 
-interface IAutofarmStratX2 {
-  function deposit(address _userAddress, uint256 _wantAmt) external returns (uint256);
+interface IAutofarmV2 {
+  function deposit(uint256 _pid, uint256 _wantAmt) external returns (uint256);
 
-  function withdraw(address _userAddress, uint256 _wantAmt) external returns (uint256);
+  function withdraw(uint256 _pid, uint256 _wantAmt) external returns (uint256);
 
-  function balanceOf(address _account) external view returns (uint256);
+  //Returns underlying balance in strategies
+  function stakedWantTokens(uint256 _pid, address _user) external view returns (uint256);
 }
 
 /**
  * @title Autofarm ERC4626 Contract
- * @notice ERC4626 wrapper for Autofarm StratX2
+ * @notice ERC4626 wrapper for AutofarmV2
  * @author RedVeil
  *
- * Wraps https://github.com/autofarmnetwork/AutofarmV2_CrossChain/blob/master/StratX2.sol
+ * Wraps https://github.com/autofarmnetwork/AutofarmV2_CrossChain/blob/master/AutoFarmV2.sol
+ *
  */
 contract AutofarmERC4626 is ERC4626 {
   using SafeTransferLib for ERC20;
 
   /* ========== STATE VARIABLES ========== */
-
-  IAutofarmStratX2 public immutable autofarmStrat;
+  uint256 public immutable poolId;
+  IAutofarmV2 public immutable autofarm;
 
   /* ========== CONSTRUCTOR ========== */
 
@@ -34,15 +36,17 @@ contract AutofarmERC4626 is ERC4626 {
      @param _asset The ERC20 compliant token the Vault should accept.
      @param _name The name for the vault token.
      @param _symbol The symbol for the vault token.
-     @param _autofarmStrat The Beefy Vault contract.
+     @param _autofarm The autofarm contract.
     */
   constructor(
     ERC20 _asset,
     string memory _name,
     string memory _symbol,
-    IAutofarmStratX2 _autofarmStrat
+    uint256 _poolId,
+    IAutofarmV2 _autofarm
   ) ERC4626(_asset, _name, _symbol) {
-    autofarmStrat = _autofarmStrat;
+    poolId = _poolId;
+    autofarm = _autofarm;
   }
 
   /* ========== VIEWS ========== */
@@ -50,24 +54,24 @@ contract AutofarmERC4626 is ERC4626 {
   /// @notice Calculates the total amount of underlying tokens the Vault holds.
   /// @return The total amount of underlying tokens the Vault holds.
   function totalAssets() public view override returns (uint256) {
-    return autofarmStrat.wantLockedTotal();
+    return autofarm.stakedWantTokens(poolId, address(this));
   }
 
-  /// @notice Calculates the total amount of underlying tokens the Vault holds.
-  /// @return The total amount of underlying tokens the Vault holds.
-  function balanceOfUnderlying(address _x) public view returns (uint256) {
-    _x;
-    return autofarmStrat.wantLockedTotal();
+  /// @notice Calculates the total amount of underlying tokens the user holds.
+  /// @return The total amount of underlying tokens the user holds.
+  function balanceOfUnderlying(address account) public view returns (uint256) {
+    return balanceOf(account).mulDivDown(totalAssets(), totalSupply());
   }
 
   /* ========== INTERNAL FUNCTIONS ========== */
 
-  function afterDeposit(uint256 amount, uint256 shares) internal override {
-    asset.approve(address(autofarmStrat), amount);
-    autofarmStrat.deposit(address(this), amount);
+  function afterDeposit(uint256 amount, uint256) internal override {
+    asset.approve(address(autofarm), amount);
+    autofarm.deposit(poolId, amount);
   }
 
-  function beforeWithdraw(uint256 amount, uint256 shares) internal override {
-    autofarmStrat.withdraw(address(this), shares);
+  /// @notice withdraws specified amount of underlying token if possible
+  function beforeWithdraw(uint256 amount, uint256) internal override {
+    autofarm.withdraw(poolId, amount);
   }
 }
