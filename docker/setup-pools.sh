01dcf0b1d690b985f04a8955eafa2f7ddefb15cf
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-02-15 17:53:27 +0100

    debug

--- docker/setup-pools.sh
+++ docker/setup-pools.sh
@@ -5,8 +5,8 @@ set -e
 ./wait-for-hh.sh
 
 npx hardhat pools:create-unhealthy-eth-borrow-token-collateral --network localhost
-npx hardhat pools:create-unhealthy-token-borrow-token-collateral --network localhost
-npx hardhat pools:create-unhealthy-token-borrow-eth-collateral --network localhost
+#npx hardhat pools:create-unhealthy-token-borrow-token-collateral --network localhost
+#npx hardhat pools:create-unhealthy-token-borrow-eth-collateral --network localhost
 
 npx hardhat e2e:unhealthy-pools-exist --network localhost
 
