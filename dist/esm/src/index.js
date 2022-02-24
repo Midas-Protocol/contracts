b3b9b7342800c51ef78e4be2f32283e24564acc6
Leon Niesler <leon.niesler@gmail.com>
2022-02-24 18:01:14 +0100

    updated openZeppelin upgradable-contracts

---
+++ dist/esm/src/index.js
@@ -0,0 +1,6 @@
+import Fuse from "./Fuse";
+import ERC20Abi from "./Fuse/abi/ERC20.json";
+export { Fuse };
+export { SupportedChains } from "./network";
+export { filterOnlyObjectProperties } from "./Fuse/utils";
+export { ERC20Abi };
