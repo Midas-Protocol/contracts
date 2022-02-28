5264a3b8528dc6d90d4cfeb23eb48669162c9eeb
Rahul Sethuram <rahul@sethuram.com>
2022-02-28 19:06:37 +0400

    Liquidation tests

--- test/liquidationFeeSeizing.ts
+++ test/liquidationFeeSeizing.ts
@@ -5,7 +5,6 @@ import { DeployedAsset, getPoolAssets } from "./utils/pool";
 import { setupAndLiquidatePool, setupLiquidatablePool } from "./utils/collateral";
 import {
   CErc20,
-  CEther,
   ERC20,
   FuseFeeDistributor,
   FuseSafeLiquidator,
@@ -15,17 +14,22 @@ import {
 import { expect } from "chai";
 import { FUSE_LIQUIDATION_PROTOCOL_FEE_PER_THOUSAND, FUSE_LIQUIDATION_SEIZE_FEE_PER_THOUSAND } from "./utils/config";
 import { TransactionReceipt } from "@ethersproject/abstract-provider";
+import { cERC20Conf } from "../src";
 
 describe("Protocol Liquidation Seizing", () => {
-  let tribe: DeployedAsset;
-  let eth: DeployedAsset;
+  let eth: cERC20Conf;
+
+  let deployedEth: DeployedAsset;
+  let deployedErc20One: DeployedAsset;
+
   let poolAddress: string;
   let simpleOracle: SimplePriceOracle;
   let oracle: MasterPriceOracle;
   let liquidator: FuseSafeLiquidator;
-  let tribeCToken: CErc20;
-  let ethCToken: CEther;
   let fuseFeeDistributor: FuseFeeDistributor;
+
+  let erc20OneCToken: CErc20;
+
   let tx: providers.TransactionResponse;
 
   beforeEach(async () => {
@@ -35,54 +39,73 @@ describe("Protocol Liquidation Seizing", () => {
     const assets = await getPoolAssets(poolAddress);
     const deployedAssets = await deployAssets(assets.assets, bob);
 
-    tribe = deployedAssets.find((a) => a.symbol === "TRIBE");
-    eth = deployedAssets.find((a) => a.underlying === constants.AddressZero);
+    const erc20One = assets.assets.find((a) => a.underlying !== constants.AddressZero); // find first one
+    expect(erc20One.underlying).to.be.ok;
+    const erc20Two = assets.assets.find(
+      (a) => a.underlying !== constants.AddressZero && a.underlying !== erc20One.underlying
+    ); // find second one
 
-    simpleOracle = (await ethers.getContract("SimplePriceOracle", deployer)) as SimplePriceOracle;
-    tx = await simpleOracle.setDirectPrice(tribe.underlying, "421407501053518");
+    oracle = (await ethers.getContract("MasterPriceOracle", deployer)) as MasterPriceOracle;
+    expect(erc20Two.underlying).to.be.ok;
+    const eth = assets.assets.find((a) => a.underlying === constants.AddressZero);
+
+    await oracle.add([eth.underlying, erc20One.underlying, erc20Two.underlying], Array(3).fill(simpleOracle.address));
+
+    let tx = await simpleOracle.setDirectPrice(eth.underlying, utils.parseEther("1"));
     await tx.wait();
 
-    oracle = (await ethers.getContract("MasterPriceOracle")) as MasterPriceOracle;
+    tx = await simpleOracle.setDirectPrice(erc20One.underlying, utils.parseEther("10"));
+    await tx.wait();
+
+    tx = await simpleOracle.setDirectPrice(erc20Two.underlying, utils.parseEther("0.0001"));
+    await tx.wait();
+
+    simpleOracle = (await ethers.getContract("SimplePriceOracle", deployer)) as SimplePriceOracle;
+    tx = await simpleOracle.setDirectPrice(erc20One.underlying, "421407501053518");
+    await tx.wait();
+
+    deployedEth = deployedAssets.find((a) => a.underlying === constants.AddressZero);
+    deployedErc20One = deployedAssets.find((a) => a.underlying === erc20One.underlying);
+
     fuseFeeDistributor = (await ethers.getContract("FuseFeeDistributor", deployer)) as FuseFeeDistributor;
     liquidator = (await ethers.getContract("FuseSafeLiquidator", rando)) as FuseSafeLiquidator;
-    tribeCToken = (await ethers.getContractAt("CErc20", tribe.assetAddress)) as CErc20;
-    ethCToken = (await ethers.getContractAt("CEther", eth.assetAddress)) as CEther;
+
+    erc20OneCToken = (await ethers.getContractAt("CErc20", deployedErc20One.assetAddress)) as CErc20;
   });
 
   it("should calculate the right amounts of protocol, fee, total supply after liquidation", async function () {
     this.timeout(120_000);
     const { bob, rando } = await ethers.getNamedSigners();
-    const { chainId } = await ethers.provider.getNetwork();
 
     const borrowAmount = "0.0001";
     const repayAmount = utils.parseEther(borrowAmount).div(10);
 
-    await setupLiquidatablePool(oracle, tribe, poolAddress, simpleOracle, borrowAmount);
+    await setupLiquidatablePool(oracle, deployedErc20One, poolAddress, simpleOracle, borrowAmount);
 
-    const liquidatorBalanceBefore = await tribeCToken.balanceOf(rando.address);
-    const borrowerBalanceBefore = await tribeCToken.balanceOf(bob.address);
-    const totalReservesBefore = await tribeCToken.totalReserves();
-    const totalSupplyBefore = await tribeCToken.totalSupply();
-    const feesBefore = await tribeCToken.totalFuseFees();
+    const liquidatorBalanceBefore = await erc20OneCToken.balanceOf(rando.address);
+    const borrowerBalanceBefore = await erc20OneCToken.balanceOf(bob.address);
+    const totalReservesBefore = await erc20OneCToken.totalReserves();
+    const totalSupplyBefore = await erc20OneCToken.totalSupply();
+    const feesBefore = await erc20OneCToken.totalFuseFees();
 
     tx = await liquidator["safeLiquidate(address,address,address,uint256,address,address,address[],bytes[])"](
       bob.address,
-      eth.assetAddress,
-      tribe.assetAddress,
+      deployedEth.assetAddress,
+      deployedErc20One.assetAddress,
       0,
-      tribe.assetAddress,
+      deployedErc20One.assetAddress,
       constants.AddressZero,
       [],
       [],
       { value: repayAmount, gasLimit: 10000000, gasPrice: utils.parseUnits("10", "gwei") }
     );
 
-    const exchangeRate = await tribeCToken.exchangeRateStored();
-    const borrowerBalanceAfter = await tribeCToken.balanceOf(bob.address);
-    const liquidatorBalanceAfter = await tribeCToken.balanceOf(rando.address);
-    const totalReservesAfter = await tribeCToken.totalReserves();
-    const totalSupplyAfter = await tribeCToken.totalSupply();
-    const feesAfter = await tribeCToken.totalFuseFees();
+    const exchangeRate = await erc20OneCToken.exchangeRateStored();
+    const borrowerBalanceAfter = await erc20OneCToken.balanceOf(bob.address);
+    const liquidatorBalanceAfter = await erc20OneCToken.balanceOf(rando.address);
+    const totalReservesAfter = await erc20OneCToken.totalReserves();
+    const totalSupplyAfter = await erc20OneCToken.totalSupply();
+    const feesAfter = await erc20OneCToken.totalFuseFees();
 
     expect(liquidatorBalanceAfter).to.be.gt(liquidatorBalanceBefore);
     expect(totalReservesAfter).to.be.gt(totalReservesBefore);
@@ -123,21 +146,21 @@ describe("Protocol Liquidation Seizing", () => {
     // gt because reserves get added on interest rate accrual
     expect(reservesDiffAmount).to.be.gt(protocolSeizeTokens);
   });
+
   it("should be able to withdraw fees to fuseFeeDistributor", async function () {
     this.timeout(120_000);
     const borrowAmount = "0.0001";
-    const { chainId } = await ethers.provider.getNetwork();
-    await setupAndLiquidatePool(oracle, tribe, eth, poolAddress, simpleOracle, borrowAmount, liquidator);
+    await setupAndLiquidatePool(oracle, deployedErc20One, eth, poolAddress, simpleOracle, borrowAmount, liquidator);
 
-    const feesAfterLiquidation = await tribeCToken.totalFuseFees();
+    const feesAfterLiquidation = await erc20OneCToken.totalFuseFees();
     expect(feesAfterLiquidation).to.be.gt(BigNumber.from(0));
     console.log(feesAfterLiquidation.toString(), "FEES AFTER");
 
-    tx = await tribeCToken._withdrawFuseFees(feesAfterLiquidation);
+    tx = await erc20OneCToken._withdrawFuseFees(feesAfterLiquidation);
     const receipt: TransactionReceipt = await tx.wait();
     expect(receipt.status).to.eq(1);
 
-    const feesAfterWithdrawal = await tribeCToken.totalFuseFees();
+    const feesAfterWithdrawal = await erc20OneCToken.totalFuseFees();
     expect(feesAfterLiquidation).to.be.gt(feesAfterWithdrawal);
     expect(feesAfterWithdrawal).to.eq(BigNumber.from(0));
 
