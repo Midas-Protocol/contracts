40c531c767b004216b6aa85bb683892695d9e6d7
smartdevcom <smart.dev.cop@gmail.com>
2022-03-09 02:48:32 +1000

    Set log level to ERROR

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Fuse from "./Fuse";
 import ERC20Abi from "./Fuse/abi/ERC20.json";
+import { utils } from "ethers";
+
+utils.Logger.setLogLevel(utils.Logger.levels.ERROR);
 
 export { Fuse };
 export {
