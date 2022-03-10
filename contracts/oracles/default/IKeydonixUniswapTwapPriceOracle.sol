51453b90236e83556a993f9b21f683d3268cddd5
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-03-10 13:52:04 +0200

    amending the keydonix oracle interface

--- contracts/oracles/default/IKeydonixUniswapTwapPriceOracle.sol
+++ contracts/oracles/default/IKeydonixUniswapTwapPriceOracle.sol
@@ -5,8 +5,10 @@ import "../../external/compound/ICToken.sol";
 import "../keydonix/UniswapOracle.sol";
 
 interface IKeydonixUniswapTwapPriceOracle {
+  event PriceAlreadyVerified(address indexed underlying, uint256 price, uint256 block);
+  event PriceVerified(address indexed underlying, uint256 price, uint256 block);
+
   function verifyPrice(ICToken cToken, UniswapOracle.ProofData calldata proofData) external returns (uint256, uint256);
 
-  event PriceAlreadyVerified(address indexed cToken, uint256 price, uint256 block);
-  event PriceVerified(address indexed cToken, uint256 price, uint256 block);
+  function verifyPriceUnderlying(address underlying, UniswapOracle.ProofData calldata proofData) external returns (uint256, uint256);
 }
