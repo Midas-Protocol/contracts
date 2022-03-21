625ab0bd4c367092218d04c11e6966a23c3bb326
pczek <pczek@users.noreply.github.com>
2022-03-21 21:03:41 +0100

    mirrors more Contract functions into flywheel sdk

--- src/modules/Flywheel.ts
+++ src/modules/Flywheel.ts
@@ -1,20 +1,32 @@
-import { BigNumber, BigNumberish, Contract, ContractFactory } from "ethers";
-import { Comptroller } from "../../typechain/Comptroller";
-import { ERC20 } from "../../typechain/ERC20";
-import { RewardsDistributorDelegate } from "../../typechain/RewardsDistributorDelegate";
-import { FuseBaseConstructor } from "../Fuse/types";
-import FlyWheelCoreArtifact from "../../artifacts/contracts/flywheel/FlyWheelCore.sol/FlyWheelCore.json";
-import { FlywheelCore, FlywheelCore__factory } from "../../typechain";
+import { BigNumber, Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
+import FlywheelCoreArtifact from "../../artifacts/contracts/flywheel/FlywheelCore.sol/FlywheelCore.json";
+import FuseFlywheelLensRouterArtifact from "../../artifacts/contracts/flywheel/fuse-compatibility/FuseFlywheelLensRouter.sol/FuseFlywheelLensRouter.json";
+import FlywheelDynamicRewardsArtifact from "../../artifacts/contracts/flywheel/rewards/FlywheelDynamicRewards.sol/FlywheelDynamicRewards.json";
+import FlywheelStaticRewardsArtifact from "../../artifacts/contracts/flywheel/rewards/FlywheelStaticRewards.sol/FlywheelStaticRewards.json";
+import IFlywheelRewardsArtifact from "../../artifacts/contracts/flywheel/interfaces/IFlywheelRewards.sol/IFlywheelRewards.json";
+import {
+  FlywheelCore,
+  FlywheelCore__factory,
+  FlywheelStaticRewards,
+  FlywheelStaticRewards__factory,
+} from "../../typechain";
+import { Comptroller } from "../../typechain/Comptroller";
+import { FuseBaseConstructor } from "../Fuse/types";
 
 export function withFlywheel<TBase extends FuseBaseConstructor>(Base: TBase) {
   return class Flywheel extends Base {
     constructor(...args) {
       super(...args);
-      this.artifacts.FlyWheelCore = FlyWheelCoreArtifact;
+      this.artifacts.FlywheelCore = FlywheelCoreArtifact;
+      this.artifacts.FlywheelDynamicRewardsArtifact = FlywheelDynamicRewardsArtifact;
+      this.artifacts.FlywheelStaticRewards = FlywheelStaticRewardsArtifact;
+      // TODO add ass Chain Deployment!
+      this.artifacts.FuseFlywheelLensRouter = FuseFlywheelLensRouterArtifact;
+      this.artifacts.IFlywheelRewards = IFlywheelRewardsArtifact;
     }
 
-    async deployFlywheel(
+    async deployFlywheelCore(
       rewardTokenAddress: string,
       options: {
         from: string;
@@ -25,8 +37,8 @@ export function withFlywheel<TBase extends FuseBaseConstructor>(Base: TBase) {
       }
     ) {
       const flywheelCoreFactory = new ContractFactory(
-        this.artifacts.FlyWheelCore.abi,
-        this.artifacts.FlyWheelCore.bytecode,
+        this.artifacts.FlywheelCore.abi,
+        this.artifacts.FlywheelCore.bytecode,
         this.provider.getSigner()
       ) as FlywheelCore__factory;
       return (await flywheelCoreFactory.deploy(
@@ -36,6 +48,68 @@ export function withFlywheel<TBase extends FuseBaseConstructor>(Base: TBase) {
         options.ownerAddress || options.from,
         options.authorityAddress || ethers.constants.AddressZero
       )) as FlywheelCore;
+    }
+
+    async deployFlywheelStaticRewards(
+      rewardTokenAddress: string,
+      options: {
+        from: string;
+        flywheelCoreAddress?: string;
+        ownerAddress?: string;
+        authorityAddress?: string;
+      }
+    ) {
+      const fwStaticRewardsFactory = new ContractFactory(
+        this.artifacts.FlywheelStaticRewards.abi,
+        this.artifacts.FlywheelStaticRewards.bytecode,
+        this.provider.getSigner()
+      ) as FlywheelStaticRewards__factory;
+      return (await fwStaticRewardsFactory.deploy(
+        rewardTokenAddress,
+        options.flywheelCoreAddress || ethers.constants.AddressZero,
+        options.ownerAddress || options.from,
+        options.authorityAddress || ethers.constants.AddressZero
+      )) as FlywheelStaticRewards;
+    }
+
+    setStaticRewardInfo(
+      staticRewardsAddress: string,
+      rewardTokenAddress: string,
+      rewardInfo: FlywheelStaticRewards.RewardsInfoStruct,
+      options: { from: string }
+    ) {
+      const flywheelCoreInstance = this.#getStaticRewardsInstance(staticRewardsAddress, options);
+      return flywheelCoreInstance.functions.setRewardsInfo(rewardTokenAddress, rewardInfo);
+    }
+
+    addMarketForRewardsToFlywheelCore(flywheelCoreAddress: string, marketAddress: string, options: { from: string }) {
+      const flywheelCoreInstance = this.#getFlywheelCoreInstance(flywheelCoreAddress, options);
+      return flywheelCoreInstance.functions.addMarketForRewards(marketAddress);
+    }
+
+    addFlywheelCoreToPool(flywheelCoreAddress: string, poolAddress: string, options: { from: string }) {
+      const comptrollerInstance = new Contract(
+        poolAddress,
+        this.artifacts.Comptroller.abi,
+        this.provider.getSigner(options.from)
+      ) as Comptroller;
+      return comptrollerInstance.functions._addRewardsDistributor(flywheelCoreAddress);
+    }
+
+    #getStaticRewardsInstance(flywheelCoreAddress: string, options: { from: string }) {
+      return new Contract(
+        flywheelCoreAddress,
+        this.artifacts.FlywheelStaticRewards.abi,
+        this.provider.getSigner(options.from)
+      ) as FlywheelStaticRewards;
+    }
+
+    #getFlywheelCoreInstance(flywheelCoreAddress: string, options: { from: string }) {
+      return new Contract(
+        flywheelCoreAddress,
+        this.artifacts.FlywheelCore.abi,
+        this.provider.getSigner(options.from)
+      ) as FlywheelCore;
     }
   };
 }
