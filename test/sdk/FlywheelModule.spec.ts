f63553334dfadc2ef779e8b36c19990287cbe1e5
pczek <pczek@users.noreply.github.com>
2022-04-07 11:39:38 +0200

    deployed correct fixture tag

--- test/sdk/FlywheelModule.spec.ts
+++ test/sdk/FlywheelModule.spec.ts
@@ -23,7 +23,7 @@ describe("FlywheelModule", function () {
   this.beforeEach(async () => {
     ({ chainId } = await ethers.provider.getNetwork());
     if (chainId === 1337) {
-      await deployments.fixture();
+      await deployments.fixture("prod");
     }
     await setUpPriceOraclePrices();
     const { deployer } = await ethers.getNamedSigners();
