fcfac7748dd0d2fa6f36ecd702ef59553f9e0e4b
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-04-11 11:01:09 +0300

    gov and ve tokens

--- contracts/gauges/VeMDSToken.sol
+++ contracts/gauges/VeMDSToken.sol
@@ -7,54 +7,52 @@ import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.s
 import "flywheel-v2/token/ERC20Gauges.sol";
 
 import "./GaugesController.sol";
+import "../utils/TOUCHToken.sol";
 
 // TODO integrate with FlywheelGaugeRewards
 // TODO research ERC20VotesUpgradeable
-contract VeMDSToken is Initializable, ERC20Gauges {
-  mapping(address => uint256) public stakingStartedTime;
-  mapping(address => uint256) public stakes;
+contract VeMDSToken is ERC20Gauges {
   IERC20Upgradeable public mdsToken;
 
   constructor(
     uint32 _gaugeCycleLength,
     uint32 _incrementFreezeWindow,
     address _owner,
-    Authority _authority
+    Authority _authority,
+    address _mdsTokenAddress
 )
   ERC20Gauges(_gaugeCycleLength, _incrementFreezeWindow)
   Auth(_owner, _authority)
   ERC20("voting escrow MDS", "veMDS", 18)
   {
-
-  }
-
-  function initialize(address _mdsTokenAddress) public initializer {
     mdsToken = IERC20Upgradeable(_mdsTokenAddress); // TODO typed contract param
   }
 
-  // TODO move in gov token
-  function stake(uint256 amount) public {
-    transferFrom(msg.sender, address(this), amount);
-    stakingStartedTime[msg.sender] = block.timestamp;
-    stakes[msg.sender] = amount;
+  modifier onlyMdsToken() {
+    require(msg.sender == address(mdsToken), "only the mdstoken can mint");
+    _;
   }
 
-  function votingPowerOf(address account) public view returns (uint) {
-    uint stakingStartedTime = stakingStartedTime[account];
-    if (stakingStartedTime == 0) {
-      return 0;
-    } else {
-      uint _stake = stakes[account];
-      uint hoursSinceStaked = (block.timestamp - stakingStartedTime) % 3600;
-      if (hoursSinceStaked < 7143) { // 7143 * 0.014 = 100.002
-        // percentage unlocked = hours since staked * 0.014
-        return (_stake * hoursSinceStaked * 14) / 100000;
-      } else {
-        // 298 * 24 = 7152
-        // during day 298 voting power becomes 100% of the staked MDS
-        return _stake;
-      }
-    }
+  /// @notice thrown when incrementing over a users free weight.
+  error TransferNotSupported();
+
+  function mint(address to, uint256 amount) public onlyMdsToken {
+    _mint(to, amount);
   }
 
+  function burn(address from, uint256 amount) public onlyMdsToken {
+    _burn(from, amount);
+  }
+
+  function transfer(address, uint256) public virtual override returns (bool) {
+    revert("Transfer not supported");
+  }
+
+  function transferFrom(
+    address,
+    address,
+    uint256
+  ) public virtual override returns (bool) {
+    revert("Transfer not supported");
+  }
 }
--- contracts/utils/TOUCHToken.sol
+++ contracts/utils/TOUCHToken.sol
@@ -3,9 +3,51 @@ pragma solidity ^0.8.0;
 // SPDX-License-Identifier: UNLICENSED
 
 import { ERC20 } from "solmate/tokens/ERC20.sol";
+import "../gauges/VeMDSToken.sol";
 
 contract TOUCHToken is ERC20 {
-  constructor(uint256 initialSupply, address tokenOwner) ERC20("Midas TOUCH Token", "TOUCH", 18) {
-    _mint(tokenOwner, initialSupply);
+  mapping(address => uint256) public stakingStartedTime;
+  mapping(address => uint256) public stakes;
+  address public totalStaked;
+  VeMDSToken veToken;
+
+  constructor(uint256 _initialSupply, VeMDSToken _veToken) ERC20("Midas TOUCH Token", "TOUCH", 18) {
+    _mint(msg.sender, _initialSupply);
+    veToken = _veToken;
+  }
+
+  function stake(uint256 amount) public {
+    transferFrom(msg.sender, address(this), amount);
+    stakingStartedTime[msg.sender] = block.timestamp;
+    stakes[msg.sender] = amount;
+    totalStaked += amount;
+
+    uint256 maxCurrentVotingPower = votingPowerOf(msg.sender);
+    uint256 currentBalanceOfVeTokens = veToken.balanceOf(msg.sender);
+    uint256 amountToMint = maxCurrentVotingPower - currentBalanceOfVeTokens;
+
+    veToken.mint(msg.sender, amountToMint);
+  }
+
+  function unstake(uint256 amount) public {
+    stakingStartedTime[msg.sender] = 0;// ?
+  }
+
+  function votingPowerOf(address account) public view returns (uint) {
+    uint stakingStartedTime = stakingStartedTime[account];
+    if (stakingStartedTime == 0) {
+      return 0;
+    } else {
+      uint _stake = stakes[account];
+      uint hoursSinceStaked = (block.timestamp - stakingStartedTime) % 3600;
+      if (hoursSinceStaked < 7143) { // 7143 * 0.014 = 100.002
+        // percentage unlocked = hours since staked * 0.014
+        return (_stake * hoursSinceStaked * 14) / 100000;
+      } else {
+        // 298 * 24 = 7152
+        // during day 298 voting power becomes 100% of the staked MDS
+        return _stake;
+      }
+    }
   }
 }
