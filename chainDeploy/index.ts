09b03a81209a4910a74df0d419d3d7f6dd2f8a57
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-04-13 13:09:31 +0200

    chainDeploy for moonbase, chainid

--- chainDeploy/index.ts
+++ chainDeploy/index.ts
@@ -2,6 +2,8 @@ import { deployConfig as deployConfig56, deploy as deploy56 } from "./mainnets/b
 import { deployConfig as deployConfig9000, deploy as deploy9000 } from "./testnets/evmostestnet";
 import { deployConfig as deployConfig97, deploy as deploy97 } from "./testnets/chapel";
 import { deployConfig as deployConfig42, deploy as deploy42 } from "./testnets/kovan";
+import { deployConfig as deployConfig1287, deploy as deploy1287 } from "./testnets/moonabase";
+
 import { ChainDeployConfig } from "./helpers";
 import { deployConfig as deployConfig1337, deploy as deploy1337 } from "./local/local";
 
@@ -16,6 +18,7 @@ export const chainDeployConfig: Record<number, { config: ChainDeployConfig; depl
   42: { config: deployConfig42, deployFunc: deploy42 },
   // local
   1337: { config: deployConfig1337, deployFunc: deploy1337 },
+  1287: { config: deployConfig1287, deployFunc: deploy1287 },
 };
 
 export * from "./helpers/types";
