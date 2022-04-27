b79b14041f3a103818e0fe8d6e5935799ba0f737
jupiteruwdev <harutoinoue8@gmail.com>
2022-04-27 12:31:10 -0400

    github workflow fork block number updated.

--- contracts/test/config/BaseTest.t.sol
+++ contracts/test/config/BaseTest.t.sol
@@ -56,20 +56,6 @@ abstract contract BaseTest is DSTest {
     chainConfigs[BSC_MAINNET].coins[0] = IERC20Upgradeable(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
     chainConfigs[BSC_MAINNET].coins[1] = IERC20Upgradeable(0x316622977073BBC3dF32E7d2A9B3c77596a0a603);
 
-    chainConfigs[MOONBEAM_MAINNET] = ChainConfig({
-      pool: IERC20Upgradeable(0x0000000000000000000000000000000000000000),
-      lpToken: IERC20Upgradeable(0x0000000000000000000000000000000000000000),
-      lpTokenWhale: 0x0000000000000000000000000000000000000000,
-      coins: new IERC20Upgradeable[](2),
-      weth: WETH(payable(0x0000000000000000000000000000000000000000)),
-      curveLPTokenPriceOracleNoRegistry: CurveLpTokenPriceOracleNoRegistry(0x0000000000000000000000000000000000000000),
-      chainlinkOracle: ChainlinkPriceOracleV2(0x0000000000000000000000000000000000000000),
-      synthereumLiquiditiyPool: ISynthereumLiquidityPool(0x0000000000000000000000000000000000000000),
-      masterPriceOracle: MasterPriceOracle(0x0000000000000000000000000000000000000000),
-      twapOraclesFactory: UniswapTwapPriceOracleV2Factory(0x0000000000000000000000000000000000000000),
-      uniswapV2Factory: IUniswapV2Factory(0x0000000000000000000000000000000000000000)
-    });
-
     chainConfigs[BSC_CHAPEL] = ChainConfig({
       pool: IERC20Upgradeable(0x160CAed03795365F3A589f10C379FfA7d75d4E76),
       lpToken: IERC20Upgradeable(0xaF4dE8E872131AE328Ce21D909C74705d3Aaf452),
