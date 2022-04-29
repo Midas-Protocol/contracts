dd6acd09afa8b8cc2d6eecc6b8deb3d5125458b2
jupiteruwdev <harutoinoue8@gmail.com>
2022-04-29 08:09:07 -0400

    beam strategy updated.

--- contracts/compound/strategies/BeamERC4626.sol
+++ contracts/compound/strategies/BeamERC4626.sol
@@ -2,24 +2,27 @@
 pragma solidity ^0.8.10;
 
 import { ERC4626 } from "../../utils/ERC4626.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { SafeTransferLib } from "solmate/utils/SafeTransferLib.sol";
 import { FixedPointMathLib } from "../../utils/FixedPointMathLib.sol";
 import { FlywheelCore } from "flywheel-v2/FlywheelCore.sol";
 
-interface IBeamChef {
+interface IBeamVault {
   // Info of each user.
-  struct UserInfo {
-      uint256 amount; // How many LP tokens the user has provided.
-      uint256 rewardDebt; // Reward debt. See explanation below.
-      uint256 rewardLockedUp; // Reward locked up.
-      uint256 nextHarvestUntil; // When can the user harvest again.
-  }
-  
-  mapping(uint256 => mapping(address => UserInfo)) public userInfo;
 
-  function deposit(uint256 _pid, uint256 _amount) public;
+  function userInfo(uint256 _pid, address _address)
+    external
+    view
+    returns (
+      uint256,
+      uint256,
+      uint256,
+      uint256
+    );
 
-  function withdraw(uint256 _pid, uint256 _amount) public;
+  function deposit(uint256 _pid, uint256 _amount) external;
+
+  function withdraw(uint256 _pid, uint256 _amount) external;
 }
 
 contract BeamERC4626 is ERC4626 {
@@ -28,7 +31,7 @@ contract BeamERC4626 is ERC4626 {
 
   /* ========== STATE VARIABLES ========== */
 
-  IBeamChef public immutable BEAM_CHEF;
+  IBeamVault public immutable BEAM_VAULT;
   FlywheelCore public immutable FLYWHEEL_CORE;
   uint256 public immutable POOL_ID;
 
@@ -50,14 +53,14 @@ contract BeamERC4626 is ERC4626 {
     string memory _symbol,
     uint256 _poolId,
     ERC20 _glintToken,
-    IBeamChef _beamChef,
-    FlyWheelCore _flyWheel
+    IBeamVault _beamChef,
+    FlywheelCore _flyWheel
   ) ERC4626(_asset, _name, _symbol) {
-    BEAM_CHEF = _beamChef;
+    BEAM_VAULT = _beamChef;
     POOL_ID = _poolId;
     FLYWHEEL_CORE = _flyWheel;
 
-    asset.approve(address(BEAM_CHEF), type(uint256).max);
+    asset.approve(address(BEAM_VAULT), type(uint256).max);
     _glintToken.approve(address(_flyWheel.flywheelRewards()), type(uint256).max);
   }
 
@@ -66,7 +69,8 @@ contract BeamERC4626 is ERC4626 {
   /// @notice Calculates the total amount of underlying tokens the Vault holds.
   /// @return The total amount of underlying tokens the Vault holds.
   function totalAssets() public view override returns (uint256) {
-    return BEAM_CHEF.userInfo(POOL_ID, address(this));
+    (uint256 amount, , , ) = BEAM_VAULT.userInfo(POOL_ID, address(this));
+    return amount;
   }
 
   /// @notice Calculates the total amount of underlying tokens the account holds.
@@ -78,10 +82,10 @@ contract BeamERC4626 is ERC4626 {
   /* ========== INTERNAL FUNCTIONS ========== */
 
   function afterDeposit(uint256 amount, uint256) internal override {
-    BEAM_CHEF.deposit(POOL_ID, amount);
+    BEAM_VAULT.deposit(POOL_ID, amount);
   }
 
   function beforeWithdraw(uint256, uint256 shares) internal override {
-    BEAM_CHEF.withdraw(POOL_ID, shares);
+    BEAM_VAULT.withdraw(POOL_ID, shares);
   }
 }
