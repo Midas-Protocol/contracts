5eea50fa788e4e01af965b4e8aa0129c965065a8
RedVeil <leon.niesler@gmail.com>
2022-05-19 12:49:29 +0200

    CErc20PluginRewardsDelegate uses _becomeImplementation from CErc20PluginDelegate

--- contracts/compound/CErc20PluginRewardsDelegate.sol
+++ contracts/compound/CErc20PluginRewardsDelegate.sol
@@ -4,19 +4,6 @@ pragma solidity >=0.8.0;
 import "./CErc20PluginDelegate.sol";
 
 contract CErc20PluginRewardsDelegate is CErc20PluginDelegate {
-    /**
-     * @notice Delegate interface to become the implementation
-     * @param data The encoded arguments for becoming
-     */
-    function _becomeImplementation(bytes calldata data) external override {
-        require(msg.sender == address(this) || hasAdminRights());
-
-        address _plugin = abi.decode(data, (address));
-
-        plugin = IERC4626(_plugin);
-        EIP20Interface(underlying).approve(_plugin, type(uint256).max);
-    }
-
     /// @notice A reward token claim function
     /// to be overriden for use cases where rewardToken needs to be pulled in
     function claim() external {}
