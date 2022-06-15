431025359c6139328ad476a2591441e7ac039349
jupiteruwdev <harutoinoue8@gmail.com>
2022-06-15 10:24:43 -0400

    minor issue fixed.

--- contracts/compound/strategies/MidasERC4626.sol
+++ contracts/compound/strategies/MidasERC4626.sol
@@ -6,15 +6,11 @@ import "solmate/tokens/ERC20.sol";
 import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 abstract contract MidasERC4626 is ERC4626, Ownable {
-  address public owner;
-
   constructor(
     ERC20 _asset,
     string memory _name,
     string memory _symbol
   ) ERC4626(_asset, _name, _symbol) {
-    owner = _msgSender();
-    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
   }
 
   // function withdrawERC20(address _token) external adminOnly {
