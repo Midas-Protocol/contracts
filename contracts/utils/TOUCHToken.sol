775ea91cdee2c35034f2a2ce9d2e79535b3be880
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-06-30 14:12:47 +0300

    prettier fixes

--- contracts/utils/TOUCHToken.sol
+++ contracts/utils/TOUCHToken.sol
@@ -4,14 +4,17 @@ pragma solidity >=0.8.0;
 import { ERC20 } from "solmate/tokens/ERC20.sol";
 
 contract TOUCHToken is ERC20 {
-
   event TransferWithMemo(address indexed from, address indexed to, uint256 amount, bytes indexed memo);
 
   constructor(address tokenOwner, uint256 initialSupply) ERC20("Midas TOUCH Token", "TOUCH", 18) {
     _mint(tokenOwner, initialSupply);
   }
 
-  function transferWithMemo(address to, uint256 amount, bytes calldata memo) public returns (bool) {
+  function transferWithMemo(
+    address to,
+    uint256 amount,
+    bytes calldata memo
+  ) public returns (bool) {
     // checking the transfer() return value is redundant since it always reverts on failure
     transfer(to, amount);
 
