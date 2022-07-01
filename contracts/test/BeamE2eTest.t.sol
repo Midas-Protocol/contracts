6fb51c354753467ad494bfc3aa1a315831dad2a6
jupiteruwdev <harutoinoue8@gmail.com>
2022-07-01 14:17:51 -0400

    beam lp token e2e test updated.

--- contracts/test/BeamE2eTest.t.sol
+++ contracts/test/BeamE2eTest.t.sol
@@ -16,7 +16,7 @@ import { IComptroller } from "../external/compound/IComptroller.sol";
 import { FusePoolLensSecondary } from "../FusePoolLensSecondary.sol";
 import { UniswapLpTokenLiquidator } from "../liquidators/UniswapLpTokenLiquidator.sol";
 
-contract MockBeam is MockERC20 {
+contract MockBeamERC20 is MockERC20 {
   constructor() MockERC20("test", "test", 8) {}
 
   function deposit() external payable {}
@@ -27,11 +27,34 @@ contract BeamE2eTest is WithPool, BaseTest {
   StdStorage internal stdstore;
   
   address wToken = 0xAcc15dC74880C9944775448304B263D191c6077F;
+  address mPriceOracle = 0x14C15B9ec83ED79f23BF71D51741f58b69ff1494;
   address uniswapRouter = 0x96b244391D98B62D19aE89b1A4dCcf0fc56970C7;
+  address USDC = 0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b;
+  address accountOne = address(1);
+  address accountTwo = address(2);
+  address joy = 0x33Ad49856da25b8E2E2D762c411AEda0D1727918;
+  address bob = 0x739ca6D71365a08f584c8FC4e1029045Fa8ABC4B;
+
+  struct LiquidationData {
+    address[] cTokens;
+    uint256 oraclePrice;
+    FusePoolLens.FusePoolAsset[] assetsData;
+    FusePoolLens.FusePoolAsset[] assetsDataAfter;
+    IRedemptionStrategy[] strategies;
+    UniswapLpTokenLiquidator lpLiquidator;
+    address[] swapToken0Path;
+    address[] swapToken1Path;
+    bytes[] abis;
+    CToken[] allMarkets;
+    FuseSafeLiquidator liquidator;
+    MockERC4626 erc4626;
+    MockBeamERC20 asset;
+  }
+
 
   constructor()
     WithPool(
-      MasterPriceOracle(0x14C15B9ec83ED79f23BF71D51741f58b69ff1494),
+      MasterPriceOracle(mPriceOracle),
       MockERC20(0x99588867e817023162F4d4829995299054a5fC57)
     )
   {}
@@ -84,128 +107,143 @@ contract BeamE2eTest is WithPool, BaseTest {
   }
 
   function testBeamCErc20Liquidation() public shouldRun(forChains(MOONBEAM_MAINNET)) {
-    // vm.roll(1);
-    MockERC4626 erc4626 = new MockERC4626(ERC20(address(underlyingToken)));
-    MockBeam beam = MockBeam(wToken);
+    LiquidationData memory vars;
+    vars.erc4626 = new MockERC4626(ERC20(address(underlyingToken)));
+    vars.asset = MockBeamERC20(USDC);
 
-    deployCErc20PluginDelegate(erc4626, 0.9e18);
-    deployCErc20Delegate(address(beam), "BNB", "bnb", 0.9e18);
+    deployCErc20PluginDelegate(vars.erc4626, 0.9e18);
+    deployCErc20Delegate(address(vars.asset), "BNB", "bnb", 0.1e18);
 
-    CToken[] memory allMarkets = comptroller.getAllMarkets();
-    CErc20PluginDelegate cToken = CErc20PluginDelegate(address(allMarkets[0]));
+    vars.allMarkets = comptroller.getAllMarkets();
+    CErc20PluginDelegate cTokenLP = CErc20PluginDelegate(address(vars.allMarkets[0]));
+    CErc20Delegate cToken = CErc20Delegate(address(vars.allMarkets[1]));
 
-    cToken._setImplementationSafe(address(cErc20PluginDelegate), false, abi.encode(address(erc4626)));
-
-    CErc20Delegate cBeamToken = CErc20Delegate(address(allMarkets[1]));
-
-    address[] memory cTokens = new address[](2);
-    cTokens[0] = address(cToken);
-    cTokens[1] = address(cBeamToken);
-    comptroller.enterMarkets(cTokens);
+    cTokenLP._setImplementationSafe(address(cErc20PluginDelegate), false, abi.encode(address(vars.erc4626)));
 
     // setting up liquidator
-    liquidator = new FuseSafeLiquidator();
-    liquidator.initialize(
-      0xAcc15dC74880C9944775448304B263D191c6077F,
+    vars.liquidator = new FuseSafeLiquidator();
+    vars.liquidator.initialize(
+      wToken,
       uniswapRouter,
-      0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b,
+      USDC,
       0xcd3B51D98478D53F4515A306bE565c6EebeF1D58,
       "0xe31da4209ffcce713230a74b5287fa8ec84797c9e77e1f7cfeccea015cdc97ea"
     );
-    address accountOne = address(1);
-    address accountTwo = address(2);
 
-    FusePoolLensSecondary secondary = new FusePoolLensSecondary();
-    secondary.initialize(fusePoolDirectory);
-
-    vm.prank(0x33Ad49856da25b8E2E2D762c411AEda0D1727918);
+    // Tokens supply
+    vm.prank(joy);
     underlyingToken.transfer(accountTwo, 1000e18);
-    // Account One Supply
-    vm.deal(accountOne, 1000000000000e18);
-    vm.startPrank(accountOne);
-    beam.deposit{ value: 1000000000000e18 }();
-    vm.stopPrank();
+
+    vm.prank(bob);
+    vars.asset.transfer(accountOne, 10000000);
+
+    /* 
+     * CToken Supply
+     */
 
     // Account One Supply
     vm.startPrank(accountOne);
-    beam.approve(address(cBeamToken), 1e36);
-    cBeamToken.mint(1000e18);
+    vars.asset.approve(address(cToken), 1e36);
+    cToken.mint(10000000);
     vm.stopPrank();
 
     // Account Two Supply
     vm.startPrank(accountTwo);
-    underlyingToken.approve(address(cToken), 1e36);
-    cToken.mint(1000e18);
+    underlyingToken.approve(address(cTokenLP), 1e36);
+    cTokenLP.mint(10e18);
     vm.stopPrank();
-    assertEq(cToken.totalSupply(), 1000e18 * 5);
-    assertEq(cBeamToken.totalSupply(), 1000e18 * 5);
 
-    // Account One Borrow
+    assertEq(cTokenLP.totalSupply(), 10e18 * 5);
+    assertEq(cToken.totalSupply(), 10000000 * 5);
+
+    /**
+     * Adding ctokens to collateral.
+     */
+
+    vars.cTokens = new address[](1);
+
     vm.startPrank(accountTwo);
-    underlyingToken.approve(address(cToken), 1e36);
-    cBeamToken.borrow(10);
+    vars.cTokens[0] = address(cTokenLP);
+    comptroller.enterMarkets(vars.cTokens);
     vm.stopPrank();
-    assertEq(cToken.totalBorrows(), 10);
-    {
-      uint256 price1 = priceOracle.getUnderlyingPrice(ICToken(address(cToken)));
 
+    vm.startPrank(accountOne);
+    vars.cTokens[0] = address(cToken);
+    comptroller.enterMarkets(vars.cTokens);
+    vm.stopPrank();
+  
+    
+    /**
+     * Borrowing
+     */
+
+    vm.prank(accountTwo);
+    cToken.borrow(100000);
+
+    assertEq(cToken.totalBorrows(), 100000);
+
+    /**
+     * Updating oracle price
+     */
+    {
+      vars.oraclePrice = priceOracle.getUnderlyingPrice(ICToken(address(cToken)));
       vm.mockCall(
-        0x14C15B9ec83ED79f23BF71D51741f58b69ff1494,
+        mPriceOracle,
         abi.encodeWithSelector(priceOracle.getUnderlyingPrice.selector, ICToken(address(cToken))),
-        abi.encode(price1 * 10)
+        abi.encode(vars.oraclePrice * 40)
       );
     }
 
     {
-      IRedemptionStrategy[] memory strategies = new IRedemptionStrategy[](1);
-      UniswapLpTokenLiquidator lpLiquidator = new UniswapLpTokenLiquidator();
-      strategies[0] = lpLiquidator;
-      address[] memory swapToken0Path = new address[](2);
-      swapToken0Path[0] = IUniswapV2Pair(address(underlyingToken)).token0();
-      swapToken0Path[1] = IUniswapV2Pair(address(underlyingToken)).token1();
-      address[] memory swapToken1Path = new address[](2);
-      swapToken0Path[1] = IUniswapV2Pair(address(underlyingToken)).token0();
-      swapToken0Path[0] = IUniswapV2Pair(address(underlyingToken)).token1();
-      
-      bytes[] memory abis = new bytes[](1);
-      abis[0] = abi.encode(
+      vars.strategies = new IRedemptionStrategy[](1);
+      vars.lpLiquidator = new UniswapLpTokenLiquidator();
+
+      /**
+       * Whitelisting lp token redemption strategy
+       */
+      vars.liquidator._whitelistRedemptionStrategy(vars.lpLiquidator, true);
+
+      vars.strategies[0] = vars.lpLiquidator;
+      vars.swapToken0Path = new address[](2);
+      vars.swapToken1Path = new address[](0);
+      vars.abis = new bytes[](1);
+      vars.swapToken0Path[0] = IUniswapV2Pair(address(underlyingToken)).token0();
+      vars.swapToken0Path[1] = IUniswapV2Pair(address(underlyingToken)).token1();
+      vars.abis[0] = abi.encode(
         IUniswapV2Router02(uniswapRouter),
-        swapToken0Path,
-        swapToken1Path
+        vars.swapToken0Path,
+        vars.swapToken1Path
       );
 
-      vm.startPrank(accountOne);
-      // FusePoolLens.FusePoolAsset[] memory assetsData = poolLens.getPoolAssetsWithData(IComptroller(address(comptroller)));
-      // uint256 beamBalance = cBeamToken.balanceOf(accountOne);
+      vm.startPrank(accountTwo);
+      vars.assetsData = poolLens.getPoolAssetsWithData(IComptroller(address(comptroller)));
+      uint256 beamBalance = cTokenLP.balanceOf(accountTwo);
 
-      // emit log_uint(beamBalance);
-      
-      underlyingToken.approve(address(cToken), 1);
-      liquidator.safeLiquidateToTokensWithFlashLoan(
-        accountOne,
-        1,
+      /**
+       * Liquidation
+       */
+      vars.liquidator.safeLiquidateToTokensWithFlashLoan(
+        accountTwo,
+        400,
         ICErc20(address(cToken)),
-        ICErc20(address(cBeamToken)),
+        ICErc20(address(cTokenLP)),
         0,
         address(0),
         IUniswapV2Router02(uniswapRouter),
         IUniswapV2Router02(uniswapRouter),
-        strategies,
-        abis,
+        vars.strategies,
+        vars.abis,
         0
       );
+      vars.assetsDataAfter = poolLens.getPoolAssetsWithData(
+        IComptroller(address(comptroller))
+      );
+
+      uint256 beamBalanceAfter = cTokenLP.balanceOf(accountTwo);
+
+      assertGt(beamBalance, beamBalanceAfter);
+      assertGt(vars.assetsData[0].supplyBalance, vars.assetsDataAfter[0].supplyBalance);
     }
-
-    // FusePoolLens.FusePoolAsset[] memory assetsDataAfter = poolLens.getPoolAssetsWithData(
-    //   IComptroller(address(comptroller))
-    // );
-
-    // uint256 beamBalanceAfter = cBeamToken.balanceOf(accountOne);
-
-    // assertGt(beamBalance, beamBalanceAfter);
-    // assertGt(assetsData[1].supplyBalance, assetsDataAfter[1].supplyBalance);
-
-    // vm.stopPrank();
   }
 
   function testBeamDeployCErc20PluginDelegate() public shouldRun(forChains(MOONBEAM_MAINNET)) {
@@ -238,62 +276,4 @@ contract BeamE2eTest is WithPool, BaseTest {
     assertEq(balance, 10e18 - 1000);
     assertEq(underlyingToken.balanceOf(address(this)), 100e18 - 10e18 + 1000);
   }
-
-  // function testBeamDeployCErc20PluginRewardsDelegate() public shouldRun(forChains(MOONBEAM_MAINNET)) {
-  //   MockERC20 rewardToken = new MockERC20("RewardToken", "RT", 18);
-  //   FuseFlywheelDynamicRewards rewards;
-  //   FuseFlywheelCore flywheel = new FuseFlywheelCore(
-  //     underlyingToken,
-  //     IFlywheelRewards(address(0)),
-  //     IFlywheelBooster(address(0)),
-  //     address(this),
-  //     Authority(address(0))
-  //   );
-  //   rewards = new FuseFlywheelDynamicRewards(flywheel, 1);
-  //   flywheel.setFlywheelRewards(rewards);
-
-  //   MockERC4626Dynamic mockERC4626Dynamic = new MockERC4626Dynamic(
-  //     ERC20(address(underlyingToken)),
-  //     FlywheelCore(address(flywheel))
-  //   );
-
-  //   ERC20 marketKey = ERC20(address(mockERC4626Dynamic));
-  //   flywheel.addStrategyForRewards(marketKey);
-
-  //   vm.roll(1);
-  //   deployCErc20PluginRewardsDelegate(mockERC4626Dynamic, flywheel, 0.9e18);
-
-  //   CToken[] memory allMarkets = comptroller.getAllMarkets();
-  //   CErc20PluginRewardsDelegate cToken = CErc20PluginRewardsDelegate(address(allMarkets[allMarkets.length - 1]));
-
-  //   cToken._setImplementationSafe(
-  //     address(cErc20PluginRewardsDelegate),
-  //     false,
-  //     abi.encode(address(mockERC4626Dynamic), address(flywheel), address(underlyingToken))
-  //   );
-  //   assertEq(address(cToken.plugin()), address(mockERC4626Dynamic));
-  //   assertEq(underlyingToken.allowance(address(cToken), address(mockERC4626Dynamic)), type(uint256).max);
-  //   assertEq(underlyingToken.allowance(address(cToken), address(flywheel)), 0);
-
-  //   cToken.approve(address(rewardToken), address(flywheel));
-  //   assertEq(rewardToken.allowance(address(cToken), address(flywheel)), type(uint256).max);
-
-  //   underlyingToken.approve(address(cToken), 1e36);
-  //   address[] memory cTokens = new address[](1);
-  //   cTokens[0] = address(cToken);
-  //   comptroller.enterMarkets(cTokens);
-  //   vm.roll(1);
-
-  //   cToken.mint(10000000);
-  //   assertEq(cToken.totalSupply(), 10000000 * 5);
-  //   assertEq(mockERC4626Dynamic.balanceOf(address(cToken)), 10000000);
-  //   assertEq(underlyingToken.balanceOf(address(mockERC4626Dynamic)), 10000000);
-  //   vm.roll(1);
-
-  //   cToken.borrow(1000);
-  //   assertEq(cToken.totalBorrows(), 1000);
-  //   assertEq(underlyingToken.balanceOf(address(mockERC4626Dynamic)), 10000000 - 1000);
-  //   assertEq(mockERC4626Dynamic.balanceOf(address(cToken)), 10000000 - 1000);
-  //   assertEq(underlyingToken.balanceOf(address(this)), 100e18 - 10000000 + 1000);
-  // }
 }
