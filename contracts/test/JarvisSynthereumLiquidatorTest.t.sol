da6c3bb9180ecf36b2fd6351ed74a5d5c5c662e2
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-08-01 18:36:32 +0300

    cleaning the jarvis test contract

--- contracts/test/JarvisSynthereumLiquidatorTest.t.sol
+++ contracts/test/JarvisSynthereumLiquidatorTest.t.sol
@@ -8,15 +8,13 @@ import { MasterPriceOracle } from "../oracles/MasterPriceOracle.sol";
 import "./config/BaseTest.t.sol";
 import "../liquidators/JarvisSynthereumLiquidator.sol";
 import "../FuseSafeLiquidator.sol";
-import "openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
-import "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 interface IMockERC20 is IERC20Upgradeable {
   function mint(address _address, uint256 amount) external;
 }
 
 contract JarvisSynthereumLiquidatorTest is BaseTest {
-  JarvisSynthereumLiquidator private liquidator;
+  JarvisSynthereumLiquidator private jarvisLiquidator;
 
   // TODO in the addresses provider?
   ISynthereumLiquidityPool synthereumLiquiditiyPool =
@@ -30,15 +28,15 @@ contract JarvisSynthereumLiquidatorTest is BaseTest {
   function setUp() public shouldRun(forChains(BSC_MAINNET)) {
     uint64 expirationPeriod = 60 * 40; // 40 mins
     bUSD = IERC20Upgradeable(ap.getAddress("bUSD"));
-    liquidator = new JarvisSynthereumLiquidator(synthereumLiquiditiyPool, expirationPeriod);
+    jarvisLiquidator = new JarvisSynthereumLiquidator(synthereumLiquiditiyPool, expirationPeriod);
   }
 
   function testRedeemToken() public shouldRun(forChains(BSC_MAINNET)) {
     vm.prank(minter);
-    jBRLToken.mint(address(liquidator), 10e18);
+    jBRLToken.mint(address(jarvisLiquidator), 10e18);
 
-    (uint256 redeemableAmount, ) = liquidator.pool().getRedeemTradeInfo(10e18);
-    (IERC20Upgradeable outputToken, uint256 outputAmount) = liquidator.redeem(jBRLToken, 10e18, "");
+    (uint256 redeemableAmount, ) = jarvisLiquidator.pool().getRedeemTradeInfo(10e18);
+    (IERC20Upgradeable outputToken, uint256 outputAmount) = jarvisLiquidator.redeem(jBRLToken, 10e18, "");
 
     // should be BUSD
     assertEq(address(outputToken), address(bUSD));
@@ -46,74 +44,20 @@ contract JarvisSynthereumLiquidatorTest is BaseTest {
   }
 
   function testEmergencyRedeemToken() public shouldRun(forChains(BSC_MAINNET)) {
-    ISynthereumLiquidityPool pool = liquidator.pool();
+    ISynthereumLiquidityPool pool = jarvisLiquidator.pool();
     address manager = pool.synthereumFinder().getImplementationAddress("Manager");
     vm.prank(manager);
     pool.emergencyShutdown();
 
     vm.prank(minter);
-    jBRLToken.mint(address(liquidator), 10e18);
+    jBRLToken.mint(address(jarvisLiquidator), 10e18);
 
-    (uint256 redeemableAmount, uint256 fee) = liquidator.pool().getRedeemTradeInfo(10e18);
-    (IERC20Upgradeable outputToken, uint256 outputAmount) = liquidator.redeem(jBRLToken, 10e18, "");
+    (uint256 redeemableAmount, uint256 fee) = jarvisLiquidator.pool().getRedeemTradeInfo(10e18);
+    (IERC20Upgradeable outputToken, uint256 outputAmount) = jarvisLiquidator.redeem(jBRLToken, 10e18, "");
 
     // should be BUSD
     assertEq(address(outputToken), address(bUSD));
     assertEq(outputAmount, redeemableAmount + fee);
-  }
-
-  // should be run at block 19821878
-  function testLiquidate2brl() public shouldRun(forChains(BSC_MAINNET)) {
-    if (block.number != 19821878) return;
-
-    FuseSafeLiquidator.LiquidateToTokensWithFlashSwapVars memory vars;
-
-    address _wtoken = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
-    address _uniswapV2router = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
-    address _stableToken = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
-    address _btcToken = 0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c;
-
-    address deployer = 0x304aE8f9300e09c8B33bb1a8AE1c14A6253a5F4D;
-    address fslAddress = 0xc9C3D317E89f4390A564D56180bBB1842CF3c99C;
-    vars.borrower = 0xD6b2095e913695DD10C071cC2F20247e921EFb8E;
-    vars.repayAmount = 103636250967557372900;
-    vars.cErc20 = ICErc20(0xa7213deB44f570646Ea955771Cc7f39B58841363); // cBUSD
-    vars.cTokenCollateral = ICErc20(0xf0a2852958aD041a9Fb35c312605482Ca3Ec17ba); // 2brl
-    vars.minProfitAmount = 0;
-    vars.exchangeProfitTo = address(vars.cTokenCollateral);
-    IUniswapV2Router02 uniswapRouter = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
-    vars.uniswapV2RouterForBorrow = uniswapRouter;
-    vars.uniswapV2RouterForCollateral = uniswapRouter;
-    vars.redemptionStrategies = new IRedemptionStrategy[](0);
-    vars.strategyData = new bytes[](0);
-    vars.debtFundingStrategies = new IFundsConversionStrategy[](0);
-    //    vars.fundsConversionStrategies[0] = new JarvisLiquidatorFunder(ISynthereumLiquidityPool(0x0fD8170Dc284CD558325029f6AEc1538c7d99f49), 60 * 40);
-    vars.debtFundingStrategiesData = new bytes[](0);
-    vars.flashSwapFundingToken = _stableToken; // bUSD
-
-    vm.prank(address(bUSD));
-    bUSD.transfer(deployer, vars.repayAmount);
-
-    vm.prank(deployer);
-    bUSD.approve(fslAddress, vars.repayAmount);
-
-    address underlyingBorrow = vars.cErc20.underlying();
-
-    FuseSafeLiquidator fsl = new FuseSafeLiquidator(); // FuseSafeLiquidator(payable(fslAddress));
-    fsl.initialize(
-      _wtoken,
-      _uniswapV2router,
-      _stableToken,
-      _btcToken,
-      "0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5"
-    );
-
-    if (vars.debtFundingStrategies.length > 0) {
-      fsl._whitelistRedemptionStrategy(vars.debtFundingStrategies[0], true);
-    }
-
-    vm.prank(deployer);
-    fsl.safeLiquidateToTokensWithFlashLoan(vars);
   }
 
   struct LiquidationData {
