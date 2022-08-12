7137587e55eadaf7fa16573fac130f0f811eea41
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-08-12 14:32:22 +0300

    uniswap library refactoring

--- contracts/external/uniswap/UniswapV2Library.sol
+++ contracts/external/uniswap/UniswapV2Library.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.0;
 
 import "./IUniswapV2Pair.sol";
+import "./IUniswapV2Factory.sol";
 
 library UniswapV2Library {
   // returns sorted token addresses, used to handle return values from pairs sorted in this order
@@ -11,27 +12,12 @@ library UniswapV2Library {
     require(token0 != address(0), "UniswapV2Library: ZERO_ADDRESS");
   }
 
-  // calculates the CREATE2 address for a pair without making any external calls
   function pairFor(
     address factory,
     address tokenA,
     address tokenB
-  ) internal pure returns (address pair) {
-    (address token0, address token1) = sortTokens(tokenA, tokenB);
-    pair = address(
-      uint160(
-        uint256(
-          keccak256(
-            abi.encodePacked(
-              hex"ff",
-              factory,
-              keccak256(abi.encodePacked(token0, token1)),
-              hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash
-            )
-          )
-        )
-      )
-    );
+  ) internal view returns (address pair) {
+    return IUniswapV2Factory(factory).getPair(tokenA, tokenB);
   }
 
   // fetches and sorts the reserves for a pair
