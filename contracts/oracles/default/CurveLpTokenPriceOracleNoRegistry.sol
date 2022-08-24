a04ccd9940da360dc363af16d6033f3590a17f4c
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-08-24 09:51:02 +0300

    allowing pools to be reregistered to fix the wrong pool address

--- contracts/oracles/default/CurveLpTokenPriceOracleNoRegistry.sol
+++ contracts/oracles/default/CurveLpTokenPriceOracleNoRegistry.sol
@@ -105,7 +105,7 @@ contract CurveLpTokenPriceOracleNoRegistry is SafeOwnableUpgradeable, BasePriceO
     address[] memory _underlyings
   ) external onlyOwner {
     address pool = poolOf[_lpToken];
-    require(pool == address(0), "This LP token is already registered.");
+//    require(pool == address(0), "This LP token is already registered.");
     poolOf[_lpToken] = _pool;
     underlyingTokens[_lpToken] = _underlyings;
   }
