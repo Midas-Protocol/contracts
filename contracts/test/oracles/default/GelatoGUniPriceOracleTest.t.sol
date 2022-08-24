08ef93981e903909015ea448c5e822a6bb0475d5
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-08-24 12:25:16 +0300

    polygon forking fixes

--- contracts/test/oracles/default/GelatoGUniPriceOracleTest.t.sol
+++ contracts/test/oracles/default/GelatoGUniPriceOracleTest.t.sol
@@ -12,13 +12,13 @@ contract GelatoGUniPriceOracleTest is BaseTest {
   MasterPriceOracle mpo;
 
   function setUp() public shouldRun(forChains(POLYGON_MAINNET)) {
+    vm.rollFork(32016397);
+
     mpo = MasterPriceOracle(ap.getAddress("MasterPriceOracle"));
     oracle = new GelatoGUniPriceOracle(address(0));
   }
 
   function testPriceGelatoGUni() public shouldRun(forChains(POLYGON_MAINNET)) {
-    vm.rollFork(32016397);
-
     address PAR_USDC_ARRAKIS_VAULT = 0xC1DF4E2fd282e39346422e40C403139CD633Aacd;
     address WBTC_WETH_ARRAKIS_VAULT = 0x590217ef04BcB96FF6Da991AB070958b8F9E77f0;
 
