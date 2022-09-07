405cfdae8f7d7b0155b4e306ab3da3b18a533a0e
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-09-07 10:55:21 +0300

    added a case to test the difference between the usdc and usdt price on polygon

--- contracts/test/oracles/default/ChainlinkOracleTest.t.sol
+++ contracts/test/oracles/default/ChainlinkOracleTest.t.sol
@@ -9,10 +9,11 @@ import "../../config/BaseTest.t.sol";
 contract ChainlinkOraclesTest is BaseTest {
   ChainlinkPriceOracleV2 oracle;
 
+  address usdNativeFeedPolygon = 0xAB594600376Ec9fD91F8e885dADF0CE036862dE0;
   address usdcPolygon = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
   address usdtPolygon = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
-  address usdNativeFeedPolygon = 0xAB594600376Ec9fD91F8e885dADF0CE036862dE0;
   address usdcFeedPolygon = 0xfE4A8cc5b5B2366C1B58Bea3858e81843581b2F7;
+  address usdtFeedPolygon = 0x0A6513e40db6EB1b165753AD52E80663aeA50545;
   ICToken usdcMarketPolygon = ICToken(0xEf335e0faC86fe2860e7fC2cc620Adad094F3eF4);
 
   address usdNativeFeedBsc = 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE;
@@ -20,10 +21,10 @@ contract ChainlinkOraclesTest is BaseTest {
   address jBRLFeedBsc = 0x5cb1Cb3eA5FB46de1CE1D0F3BaDB3212e8d8eF48;
   address usdcBsc = 0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d;
   address usdtBsc = 0x55d398326f99059fF775485246999027B3197955;
-  ICToken usdcMarketBsc = ICToken(0x8D5bE2768c335e88b71E4e913189AEE7104f01B4);
-  ICToken usdtMarketBsc = ICToken(0x1F73754c135d5B9fDE674806f43AeDfA2c7eaDb5);
   address usdtFeedBsc = 0xB97Ad0E74fa7d920791E90258A6E2085088b4320;
   address usdcFeedBsc = 0x51597f405303C4377E36123cBc172b13269EA163;
+  ICToken usdcMarketBsc = ICToken(0x8D5bE2768c335e88b71E4e913189AEE7104f01B4);
+  ICToken usdtMarketBsc = ICToken(0x1F73754c135d5B9fDE674806f43AeDfA2c7eaDb5);
 
   function setUp() public {
     oracle = ChainlinkPriceOracleV2(ap.getAddress("ChainlinkPriceOracleV2"));
@@ -84,12 +85,22 @@ contract ChainlinkOraclesTest is BaseTest {
     assertEq(price, underlyingPrice);
   }
 
-  function testUsdcUsdtDeviation() public shouldRun(forChains(BSC_MAINNET)) {
+  function testUsdcUsdtDeviationBsc() public shouldRun(forChains(BSC_MAINNET)) {
     setUpOracleFeed(usdtBsc, usdtFeedBsc);
     setUpOracleFeed(usdcBsc, usdcFeedBsc);
 
     uint256 usdtPrice = oracle.getUnderlyingPrice(usdtMarketBsc);
     uint256 usdcPrice = oracle.getUnderlyingPrice(usdcMarketBsc);
+
+    assertApproxEqAbs(usdtPrice, usdcPrice, 1e15, "usd prices differ too much");
+  }
+
+  function testUsdcUsdtDeviationPolygon() public shouldRun(forChains(POLYGON_MAINNET)) {
+    setUpOracleFeed(usdtPolygon, usdtFeedPolygon);
+    setUpOracleFeed(usdcPolygon, usdcFeedPolygon);
+
+    uint256 usdtPrice = oracle.price(usdtPolygon);
+    uint256 usdcPrice = oracle.price(usdcPolygon);
 
     assertApproxEqAbs(usdtPrice, usdcPrice, 1e15, "usd prices differ too much");
   }
