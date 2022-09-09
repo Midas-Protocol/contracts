29e1a9b8e83b4c2b70729edf0c9161c66f3d08bc
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-09-09 16:27:00 +0300

    fixing the compilation of the tests

--- contracts/test/Comptroller.t.sol
+++ contracts/test/Comptroller.t.sol
@@ -7,7 +7,7 @@ import "./config/BaseTest.t.sol";
 
 import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { MidasFlywheel } from "../midas/strategies/flywheel/MidasFlywheel.sol";
-import { Comptroller } from "../../compound/Comptroller.sol";
+import { Comptroller } from "../compound/Comptroller.sol";
 import { IFlywheelBooster } from "flywheel-v2/interfaces/IFlywheelBooster.sol";
 import { IFlywheelRewards } from "flywheel-v2/FlywheelCore.sol";
 
