53d0615de9af74e53b59606e83a8a3ff8e440cc8
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-10-20 09:09:46 +0200

    bnbx price oracle and its tests

---
+++ contracts/oracles/default/BNBxPriceOracle.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import { ERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import { ICToken } from "../../external/compound/ICToken.sol";
+import { IStakeManager } from "../../external/stader/IStakeManager.sol";
+import { ICErc20 } from "../../external/compound/ICErc20.sol";
+
+import "../../midas/SafeOwnableUpgradeable.sol";
+import "../BasePriceOracle.sol";
+
+contract BNBxPriceOracle is SafeOwnableUpgradeable, BasePriceOracle {
+  IStakeManager public stakeManager;
+  address public BNBx;
+
+  function initialize() public initializer {
+    __SafeOwnable_init();
+    stakeManager = IStakeManager(0x7276241a669489E4BBB76f63d2A43Bfe63080F2F);
+    (, address _bnbX, , ) = stakeManager.getContracts();
+    BNBx = _bnbX;
+  }
+
+  function getUnderlyingPrice(ICToken cToken) external view override returns (uint256) {
+    // Get underlying token address
+    address underlying = ICErc20(address(cToken)).underlying();
+    require(underlying == BNBx, "Invalid underlying");
+    // no need to scale as BNBx has 18 decimals
+    return _price();
+  }
+
+  function price(address underlying) external view override returns (uint256) {
+    require(underlying == BNBx, "Invalid underlying");
+    return _price();
+  }
+
+  function _price() internal view returns (uint256) {
+    uint256 oneBNB = 1e18;
+    uint256 exchangeRate = stakeManager.convertBnbXToBnb(oneBNB);
+    return exchangeRate;
+  }
+}
