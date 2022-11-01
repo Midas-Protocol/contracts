b4f9c6bc544eed9563572d3b046cc966ac3b3e74
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-11-01 10:47:38 +0200

    refactored the uni v3 funder test

--- contracts/test/UniswapV3LiquidatorFunderTest.t.sol
+++ contracts/test/UniswapV3LiquidatorFunderTest.t.sol
@@ -17,64 +17,59 @@ import "../external/uniswap/Quoter/Quoter.sol";
 import "../external/uniswap/IUniswapV3Pool.sol";
 import "../external/uniswap/ISwapRouter.sol";
 
-interface IMockERC20 is IERC20Upgradeable {
-  function mint(address _address, uint256 amount) external;
-}
-
 contract UniswapV3LiquidatorFunderTest is BaseTest, WithPool {
   UniswapV3LiquidatorFunder private uniswapv3Liquidator;
 
-  address minter = 0x68863dDE14303BcED249cA8ec6AF85d4694dea6A;
-  IMockERC20 token1;
-  IMockERC20 token2;
+  IERC20Upgradeable token1;
+  IERC20Upgradeable token2;
   IUniswapV2Router02 uniswapRouter;
 
-  address wETH;
-  address BTCB;
   uint256 poolFee;
   uint256 repayAmount;
   uint256 borrowAmount;
 
   Quoter quoter;
 
-  function setUp() public forkAtBlock(ARBITRUM_ONE, 28739891) {
-    super.setUpWithPool(
-      MasterPriceOracle(0xd4D0cA503E8befAbE4b75aAC36675Bc1cFA533D1),
-      ERC20Upgradeable(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1)
-    );
+  function afterForkSetUp() internal override {
+    if (block.chainid == ARBITRUM_ONE) {
+      setUpWithPool(
+        MasterPriceOracle(0xd4D0cA503E8befAbE4b75aAC36675Bc1cFA533D1),
+        ERC20Upgradeable(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1)
+      );
+      quoter = new Quoter(0x1F98431c8aD98523631AE4a59f267346ea31F984);
+      setUpPool("arbitrum-test", false, 0.1e18, 1.1e18);
+      uniswapv3Liquidator = new UniswapV3LiquidatorFunder();
+      uniswapRouter = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
+      token1 = IERC20Upgradeable(0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a); // GMX, 18 decimals
+      token2 = IERC20Upgradeable(0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8); // USDC, 6 decimals
+      poolFee = 3000;
+      repayAmount = 1e18; // 1.00 GMX
+      borrowAmount = 12e18; // 12.00 GMX
+
+    } else if (block.chainid == POLYGON_MAINNET) {
+      setUpWithPool(
+        MasterPriceOracle(0xb9e1c2B011f252B9931BBA7fcee418b95b6Bdc31),
+        ERC20Upgradeable(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270)
+      );
+      quoter = new Quoter(0x1F98431c8aD98523631AE4a59f267346ea31F984);
+      setUpPool("polygon-test", false, 0.1e18, 1.1e18);
+      uniswapRouter = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
+      token1 = IERC20Upgradeable(0xE2Aa7db6dA1dAE97C5f5C6914d285fBfCC32A128);
+      token2 = IERC20Upgradeable(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
+      poolFee = 500;
+      repayAmount = 11e10;
+      borrowAmount = 1e20;
+    }
+    uniswapv3Liquidator = new UniswapV3LiquidatorFunder();
   }
 
-  function testPolygon() public forkAtBlock(POLYGON_MAINNET, 35032591) {
-    setUpWithPool(
-      MasterPriceOracle(0xb9e1c2B011f252B9931BBA7fcee418b95b6Bdc31),
-      ERC20Upgradeable(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270)
-    );
-    quoter = new Quoter(0x1F98431c8aD98523631AE4a59f267346ea31F984);
-    setUpPool("polygon-test", false, 0.1e18, 1.1e18);
-    uniswapv3Liquidator = new UniswapV3LiquidatorFunder();
-    wETH = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-    BTCB = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
-    uniswapRouter = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
-    token1 = IMockERC20(0xE2Aa7db6dA1dAE97C5f5C6914d285fBfCC32A128);
-    token2 = IMockERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
-    poolFee = 500;
-    repayAmount = 11e10;
-    borrowAmount = 1e20;
+  function testPolygon() public fork(POLYGON_MAINNET) {
     testLiquidation();
   }
 
-  function testArbitrum() public forkAtBlock(ARBITRUM_ONE, 28739891) {
-    quoter = new Quoter(0x1F98431c8aD98523631AE4a59f267346ea31F984);
-    setUpPool("arbitrum-test", false, 0.1e18, 1.1e18);
-    uniswapv3Liquidator = new UniswapV3LiquidatorFunder();
-    wETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
-    BTCB = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
-    uniswapRouter = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
-    token1 = IMockERC20(0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a);
-    token2 = IMockERC20(0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8);
-    poolFee = 3000;
-    repayAmount = 9e7;
-    borrowAmount = 1e19;
+  function testArbitrum() public fork(ARBITRUM_ONE) {
+    // collateral value falls from 50 000 USD to 500 USD
+    // GMX price 42.00 USD => debt value = 12*42 = 504 USD
     testLiquidation();
   }
 
@@ -95,19 +90,22 @@ contract UniswapV3LiquidatorFunderTest is BaseTest, WithPool {
     vars.liquidator.initialize(
       ap.getAddress("wtoken"),
       address(uniswapRouter),
-      wETH, // WETH
-      BTCB, // BTCB
+      ap.getAddress("wtoken"),
+      ap.getAddress("wBTCToken"),
       "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303",
       25
     );
 
-    deployCErc20Delegate(address(token2), "token2", "token2", 0.9e18);
-    deployCErc20Delegate(address(token1), "token1", "token1", 0.9e18);
+    deployCErc20Delegate(address(token2), "token2", "token2", 0.9e18); // usdc, 6 dec
+    deployCErc20Delegate(address(token1), "token1", "token1", 0.9e18); // gmx, 18 dec
 
     vars.allMarkets = comptroller.getAllMarkets();
 
-    CErc20Delegate cToken1 = CErc20Delegate(address(vars.allMarkets[0]));
-    CErc20Delegate cToken2 = CErc20Delegate(address(vars.allMarkets[1]));
+    CErc20Delegate cToken1 = CErc20Delegate(address(vars.allMarkets[0])); // cUsdc, 6 dec
+    CErc20Delegate cToken2 = CErc20Delegate(address(vars.allMarkets[1])); // cGmx, 18 dec
+
+    assertEq(cToken1.underlying(), address(token2), "token 1 should be USDC");
+    assertEq(cToken2.underlying(), address(token1), "token 2 should be GMX");
 
     address accountOne = address(10001);
     address accountTwo = address(20002);
@@ -126,7 +124,7 @@ contract UniswapV3LiquidatorFunderTest is BaseTest, WithPool {
       comptroller.enterMarkets(vars.cTokens);
     }
     token2.approve(address(cToken1), 1e36);
-    require(cToken1.mint(5e10) == 0, "USDC mint failed");
+    require(cToken1.mint(5e10) == 0, "USDC mint failed"); // 50 000 USDC deposited
     vm.stopPrank();
 
     vm.startPrank(accountTwo);
@@ -135,9 +133,9 @@ contract UniswapV3LiquidatorFunderTest is BaseTest, WithPool {
       vars.cTokens[0] = address(cToken2);
       vars.cTokens[1] = address(cToken1);
       comptroller.enterMarkets(vars.cTokens);
+      token1.approve(address(cToken2), 1e36);
+      require(cToken2.mint(5e21) == 0, "GMX mint failed"); // 5000 GMX deposited
     }
-    token1.approve(address(cToken2), 1e36);
-    require(cToken2.mint(5e21) == 0, "GMX mint failed");
     vm.stopPrank();
 
     // set borrow enable
@@ -147,7 +145,7 @@ contract UniswapV3LiquidatorFunderTest is BaseTest, WithPool {
 
     // Account One borrow GMX
     vm.startPrank(accountOne);
-    require(cToken2.borrow(borrowAmount) == 0, "borrow failed");
+    require(cToken2.borrow(borrowAmount) == 0, "borrow failed"); // borrow 12 GMX
     vm.stopPrank();
 
     // some time passes, interest accrues and prices change
@@ -161,7 +159,7 @@ contract UniswapV3LiquidatorFunderTest is BaseTest, WithPool {
       vm.mockCall(
         address(mpo),
         abi.encodeWithSelector(mpo.getUnderlyingPrice.selector, ICToken(address(cToken1))),
-        abi.encode(priceusdc / 1000)
+        abi.encode(priceusdc / 100)
       );
     }
 
@@ -186,9 +184,9 @@ contract UniswapV3LiquidatorFunderTest is BaseTest, WithPool {
     vars.liquidator.safeLiquidateToTokensWithFlashLoan(
       FuseSafeLiquidator.LiquidateToTokensWithFlashSwapVars(
         accountOne,
-        repayAmount,
-        ICErc20(address(cToken2)),
-        ICErc20(address(cToken1)),
+        repayAmount, // repay gmx
+        ICErc20(address(cToken2)), // gmx debt
+        ICErc20(address(cToken1)), // usdc collateral
         flashSwapPair,
         0,
         address(0),
