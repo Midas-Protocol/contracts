7027e00cc246fa7197132483e3a4d30f4d05d290
coolnft <haruto@midascapital.xyz>
2022-11-07 08:26:50 -0500

    auto farm test updated.

--- contracts/test/Autofarm/AutofarmTestConfig.sol
+++ contracts/test/Autofarm/AutofarmTestConfig.sol
@@ -1,28 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.0;
 
-import { ITestConfigStorage } from "../abstracts/ITestConfigStorage.sol";
+// import { ITestConfigStorage } from "../abstracts/ITestConfigStorage.sol";
 
-struct AutofarmTestConfig {
-  address asset;
-  uint256 poolId;
-}
+// struct AutofarmTestConfig {
+//   address asset;
+//   uint256 poolId;
+// }
 
-contract AutofarmTestConfigStorage is ITestConfigStorage {
-  AutofarmTestConfig[] internal testConfigs;
+// contract AutofarmTestConfigStorage is ITestConfigStorage {
+//   AutofarmTestConfig[] internal testConfigs;
 
-  constructor() {
-    // PAR/USDC
-    testConfigs.push(
-      AutofarmTestConfig(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56, 2)
-    );
-  }
+//   constructor() {
+//     // PAR/USDC
+//     testConfigs.push(
+//       AutofarmTestConfig(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56, 2)
+//     );
+//   }
 
-  function getTestConfig(uint256 i) public view returns (bytes memory) {
-    return abi.encode(testConfigs[i].asset, testConfigs[i].poolId);
-  }
+//   function getTestConfig(uint256 i) public view returns (bytes memory) {
+//     return abi.encode(testConfigs[i].asset, testConfigs[i].poolId);
+//   }
 
-  function getTestConfigLength() public view returns (uint256) {
-    return testConfigs.length;
-  }
-}
+//   function getTestConfigLength() public view returns (uint256) {
+//     return testConfigs.length;
+//   }
+// }
