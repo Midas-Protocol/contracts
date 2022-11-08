288c786951f1bc34df72b92eecd33c93656740f7
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-11-08 13:00:39 +0200

    only the proxy admin or owner should be able to call reinitialize

--- contracts/midas/strategies/flywheel/MidasFlywheelCore.sol
+++ contracts/midas/strategies/flywheel/MidasFlywheelCore.sol
@@ -59,7 +59,7 @@ contract MidasFlywheelCore is SafeOwnableUpgradeable {
     _transferOwnership(_owner);
   }
 
-  function reinitialize() reinitializer(2) public {
+  function reinitialize() reinitializer(2) public onlyOwnerOrAdmin {
     performanceFee = 5e16; // 5%
   }
 
