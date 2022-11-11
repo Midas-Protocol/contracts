5f44fc766c1684be0e8e9e0cbf79e825e16e7d56
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-11-11 18:24:29 +0100

    fix non running tests

--- contracts/test/oracles/default/AnkrCertificateTokenPriceOracleTest.sol
+++ contracts/test/oracles/default/AnkrCertificateTokenPriceOracleTest.sol
@@ -12,36 +12,34 @@ contract AnkrCertificateTokenPriceOracleTest is BaseTest {
   address aFTMc = 0xCfC785741Dc0e98ad4c9F6394Bb9d43Cd1eF5179;
   address aBNBc = 0xE85aFCcDaFBE7F2B096f268e31ccE3da8dA2990A;
 
-  function testFTMAnkrOracle() public forkAtBlock(FANTOM_OPERA, 50767474) {
+  function testAnkrFTMOracle() public forkAtBlock(FANTOM_OPERA, 50767474) {
     setUpOracleFtm();
-    testAnkrFTM();
+    ankrFTM();
   }
 
-  function testBSCAnkrOracle() public forkAtBlock(BSC_MAINNET, 22967648) {
+  function testAnkrBSCOracle() public forkAtBlock(BSC_MAINNET, 22967648) {
     setUpOracleBsc();
-    testAnkrBSC();
+    ankrBSC();
   }
 
   function setUpOracleFtm() public {
     oracle = new AnkrCertificateTokenPriceOracle();
-    vm.prank(oracle.owner());
     oracle.initialize(aFTMc);
   }
 
   function setUpOracleBsc() public {
     oracle = new AnkrCertificateTokenPriceOracle();
-    vm.prank(oracle.owner());
     oracle.initialize(aBNBc);
   }
 
-  function testAnkrFTM() public {
+  function ankrFTM() public {
     uint256 priceAnkrFTMc = oracle.price(aFTMc);
 
     assertGt(priceAnkrFTMc, 1e18);
     assertEq(priceAnkrFTMc, 1031771264536613055);
   }
 
-  function testAnkrBSC() public {
+  function ankrBSC() public {
     uint256 priceAnkrBNBc = oracle.price(aBNBc);
 
     assertGt(priceAnkrBNBc, 1e18);
