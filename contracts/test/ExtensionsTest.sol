6d47fbe20a455b5c086870359fcfb3ae526327a9
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-11-15 13:31:16 +0200

    second extension to a mock extension

--- contracts/test/ExtensionsTest.sol
+++ contracts/test/ExtensionsTest.sol
@@ -5,15 +5,45 @@ import "./config/BaseTest.t.sol";
 
 import { DiamondExtension } from "../midas/DiamondExtension.sol";
 import { ComptrollerFirstExtension } from "../compound/ComptrollerFirstExtension.sol";
-import { ComptrollerSecondExtension } from "../compound/ComptrollerSecondExtension.sol";
 import { FuseFeeDistributor } from "../FuseFeeDistributor.sol";
-import { Comptroller } from "../compound/Comptroller.sol";
+import { Comptroller, ComptrollerV3Storage } from "../compound/Comptroller.sol";
 import { Unitroller } from "../compound/Unitroller.sol";
+
+contract MockComptrollerExtension is DiamondExtension, ComptrollerV3Storage {
+  function getFirstMarketSymbol() public view returns (string memory) {
+    return allMarkets[0].symbol();
+  }
+
+  function _setTransferPaused(bool state) public returns (bool) {
+    return false;
+  }
+
+  function _setSeizePaused(bool state) public returns (bool) {
+    return false;
+  }
+
+  // a dummy fn to test if the replacement of extension fns works
+  function getSecondMarketSymbol() public view returns (string memory) {
+    return allMarkets[1].symbol();
+  }
+
+  function _getExtensionFunctions() external view virtual override returns (bytes4[] memory) {
+    uint8 fnsCount = 4;
+    bytes4[] memory functionSelectors = new bytes4[](fnsCount);
+    functionSelectors[--fnsCount] = this._setTransferPaused.selector;
+    functionSelectors[--fnsCount] = this._setSeizePaused.selector;
+    functionSelectors[--fnsCount] = this.getFirstMarketSymbol.selector;
+    functionSelectors[--fnsCount] = this.getSecondMarketSymbol.selector;
+    require(fnsCount == 0, "use the correct array length");
+    return functionSelectors;
+  }
+}
+
 
 contract ExtensionsTest is BaseTest {
   function testExtensionReplace() public fork(BSC_MAINNET) {
     ComptrollerFirstExtension cfe = new ComptrollerFirstExtension();
-    ComptrollerSecondExtension cse = new ComptrollerSecondExtension();
+    MockComptrollerExtension mce = new MockComptrollerExtension();
     address payable jFiatPoolAddress = payable(0x31d76A64Bc8BbEffb601fac5884372DEF910F044);
 
     {
@@ -45,12 +75,12 @@ contract ExtensionsTest is BaseTest {
     {
       Comptroller asComptroller = Comptroller(jFiatPoolAddress);
       vm.prank(asComptroller.admin());
-      asComptroller._registerExtension(cse, cfe);
+      asComptroller._registerExtension(mce, cfe);
     }
 
     // assert that the replacement worked
-    ComptrollerSecondExtension asSecondExtension = ComptrollerSecondExtension(jFiatPoolAddress);
-    emit log(asSecondExtension.getSecondMarketSymbol());
-    assertEq(asSecondExtension.getSecondMarketSymbol(), "fETH-1", "market symbol does not match");
+    MockComptrollerExtension asMockExtension = MockComptrollerExtension(jFiatPoolAddress);
+    emit log(asMockExtension.getSecondMarketSymbol());
+    assertEq(asMockExtension.getSecondMarketSymbol(), "fETH-1", "market symbol does not match");
   }
 }
