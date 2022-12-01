72d87adbac347dd98cf8d48710ffd02d455a9c15
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-12-01 17:01:47 +0100

    Add polygon ankMaticC test

--- contracts/test/oracles/default/AnkrCertificateTokenPriceOracleTest.t.sol
+++ contracts/test/oracles/default/AnkrCertificateTokenPriceOracleTest.t.sol
@@ -9,6 +9,7 @@ contract AnkrCertificateTokenPriceOracleTest is BaseTest {
 
   address aFTMc = 0xCfC785741Dc0e98ad4c9F6394Bb9d43Cd1eF5179;
   address aBNBc = 0xE85aFCcDaFBE7F2B096f268e31ccE3da8dA2990A;
+  address aMATICc = 0x0E9b89007eEE9c958c0EDA24eF70723C2C93dD58;
 
   function testAnkrFTMOracle() public forkAtBlock(FANTOM_OPERA, 50767474) {
     setUpOracleFtm();
@@ -24,6 +25,19 @@ contract AnkrCertificateTokenPriceOracleTest is BaseTest {
     uint256 priceAnkrBNBc = oracle.price(aBNBc);
     assertGt(priceAnkrBNBc, 1e18);
     assertEq(priceAnkrBNBc, 1036531403670513817);
+  }
+
+  function testAnkrPolygonOracle() public forkAtBlock(POLYGON_MAINNET, 36299660) {
+    setUpOraclePolygon();
+
+    uint256 priceAnkrMATICc = oracle.price(aMATICc);
+    assertGt(priceAnkrMATICc, 1e18);
+    assertEq(priceAnkrMATICc, 1099296293061644034);
+  }
+
+  function setUpOraclePolygon() internal {
+    oracle = new AnkrCertificateTokenPriceOracle();
+    oracle.initialize(aMATICc);
   }
 
   function setUpOracleFtm() internal {
