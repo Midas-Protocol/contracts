4a7d923451621bb53cf6322d4041528a47524efd
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-12-07 14:34:36 +0200

    debugging and reverting some changes

--- contracts/test/Comptroller.t.sol
+++ contracts/test/Comptroller.t.sol
@@ -7,39 +7,31 @@ import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { MidasFlywheel } from "../midas/strategies/flywheel/MidasFlywheel.sol";
 import { Comptroller } from "../compound/Comptroller.sol";
 import { IFlywheelBooster } from "flywheel-v2/interfaces/IFlywheelBooster.sol";
-import { IFlywheelRewards } from "flywheel-v2/FlywheelCore.sol";
+import { IFlywheelRewards } from "flywheel-v2/interfaces/IFlywheelRewards.sol";
 
 contract ComptrollerTest is BaseTest {
   Comptroller internal comptroller;
-  MidasFlywheel internal flywheel;
+  MidasFlywheel internal fw;
   address internal nonOwner = address(0x2222);
 
   event Failure(uint256 error, uint256 info, uint256 detail);
 
-  function setUp() internal {
+  function setUp() public {
     comptroller = new Comptroller(payable(address(this)));
-    flywheel = new MidasFlywheel();
-    try
-      flywheel.initialize(ERC20(address(1)), IFlywheelRewards(address(2)), IFlywheelBooster(address(3)), address(this))
-    {} catch Error(string memory err) {
-      emit log(err);
-    } catch {
-      emit log("erroring on some unknown .decimals() call");
-    }
+    fw = new MidasFlywheel();
+    fw.initialize(ERC20(address(1)), IFlywheelRewards(address(2)), IFlywheelBooster(address(3)), address(this));
   }
 
   function test__setFlywheel() external {
-    setUp();
-    comptroller._addRewardsDistributor(address(flywheel));
+    comptroller._addRewardsDistributor(address(fw));
 
-    assertEq(comptroller.rewardsDistributors(0), address(flywheel));
+    assertEq(comptroller.rewardsDistributors(0), address(fw));
   }
 
   function test__setFlywheelRevertsIfNonOwner() external {
-    setUp();
     vm.startPrank(nonOwner);
     vm.expectEmit(false, false, false, true, address(comptroller));
     emit Failure(1, 2, 0);
-    comptroller._addRewardsDistributor(address(flywheel));
+    comptroller._addRewardsDistributor(address(fw));
   }
 }
