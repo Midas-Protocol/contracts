abcd4167d76338186810bce3811a7422f47610d0
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-12-07 14:45:38 +0200

    debugging and fixes

--- contracts/test/Comptroller.t.sol
+++ contracts/test/Comptroller.t.sol
@@ -3,26 +3,31 @@ pragma solidity ^0.8.0;
 
 import { BaseTest } from "./config/BaseTest.t.sol";
 
-import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { MidasFlywheel } from "../midas/strategies/flywheel/MidasFlywheel.sol";
 import { Comptroller } from "../compound/Comptroller.sol";
+
 import { IFlywheelBooster } from "flywheel-v2/interfaces/IFlywheelBooster.sol";
 import { IFlywheelRewards } from "flywheel-v2/interfaces/IFlywheelRewards.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
 
 contract ComptrollerTest is BaseTest {
   Comptroller internal comptroller;
   MidasFlywheel internal fw;
   address internal nonOwner = address(0x2222);
+  address internal admin = address(0x7777);
 
   event Failure(uint256 error, uint256 info, uint256 detail);
 
   function setUp() public {
+    vm.startPrank(admin);
     comptroller = new Comptroller(payable(address(this)));
     fw = new MidasFlywheel();
-    fw.initialize(ERC20(address(1)), IFlywheelRewards(address(2)), IFlywheelBooster(address(3)), address(this));
+    fw.initialize(ERC20(address(1)), IFlywheelRewards(address(2)), IFlywheelBooster(address(3)), admin);
+    vm.stopPrank();
   }
 
   function test__setFlywheel() external {
+    vm.prank(admin);
     comptroller._addRewardsDistributor(address(fw));
 
     assertEq(comptroller.rewardsDistributors(0), address(fw));
