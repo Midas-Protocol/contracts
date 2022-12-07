158bee8136398b5ccf803e1f19537323003f3331
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-12-07 15:44:34 +0200

    reverting changes and fixes

--- contracts/test/Comptroller.t.sol
+++ contracts/test/Comptroller.t.sol
@@ -9,34 +9,32 @@ import { Comptroller } from "../compound/Comptroller.sol";
 import { IFlywheelBooster } from "flywheel-v2/interfaces/IFlywheelBooster.sol";
 import { IFlywheelRewards } from "flywheel-v2/interfaces/IFlywheelRewards.sol";
 import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { MockERC20 } from "solmate/test/utils/mocks/MockERC20.sol";
 
 contract ComptrollerTest is BaseTest {
   Comptroller internal comptroller;
-  MidasFlywheel internal fw;
+  MidasFlywheel internal flywheel;
   address internal nonOwner = address(0x2222);
-  address internal admin = address(0x7777);
 
   event Failure(uint256 error, uint256 info, uint256 detail);
 
   function setUp() public {
-    vm.startPrank(admin);
+    ERC20 rewardToken = new MockERC20("RewardToken", "RT", 18);
+    flywheel = new MidasFlywheel();
     comptroller = new Comptroller(payable(address(this)));
-    fw = new MidasFlywheel();
-    fw.initialize(ERC20(address(1)), IFlywheelRewards(address(2)), IFlywheelBooster(address(3)), admin);
-    vm.stopPrank();
+    flywheel.initialize(rewardToken, IFlywheelRewards(address(2)), IFlywheelBooster(address(3)), address(this));
   }
 
   function test__setFlywheel() external {
-    vm.prank(admin);
-    comptroller._addRewardsDistributor(address(fw));
+    comptroller._addRewardsDistributor(address(flywheel));
 
-    assertEq(comptroller.rewardsDistributors(0), address(fw));
+    assertEq(comptroller.rewardsDistributors(0), address(flywheel));
   }
 
   function test__setFlywheelRevertsIfNonOwner() external {
     vm.startPrank(nonOwner);
     vm.expectEmit(false, false, false, true, address(comptroller));
     emit Failure(1, 2, 0);
-    comptroller._addRewardsDistributor(address(fw));
+    comptroller._addRewardsDistributor(address(flywheel));
   }
 }
