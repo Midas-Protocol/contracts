ee504cc1419ae32b436643d3d718817cdb91fff7
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-12-12 23:45:17 +0200

    fixing more tests

--- contracts/test/MaxWithdrawTest.t.sol
+++ contracts/test/MaxWithdrawTest.t.sol
@@ -33,6 +33,7 @@ contract MaxWithdrawTest is WithPool {
     address[] cTokens;
     CTokenInterface[] allMarkets;
     MockAsset bnb;
+    MockAsset mimo;
     MockAsset usdc;
   }
 
@@ -143,10 +144,10 @@ contract MaxWithdrawTest is WithPool {
 
     LiquidationData memory vars;
     vm.roll(1);
-    vars.bnb = MockAsset(0xADAC33f543267c4D59a8c299cF804c303BC3e4aC);
+    vars.mimo = MockAsset(0xADAC33f543267c4D59a8c299cF804c303BC3e4aC);
     vars.usdc = MockAsset(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
 
-    deployCErc20Delegate(address(vars.bnb), "MIMO", "mimo", 0.9e18);
+    deployCErc20Delegate(address(vars.mimo), "MIMO", "mimo", 0.9e18);
     deployCErc20Delegate(address(vars.usdc), "USDC", "usdc", 0.9e18);
 
     vars.allMarkets = comptroller.getAllMarkets();
@@ -163,16 +164,16 @@ contract MaxWithdrawTest is WithPool {
     FusePoolLensSecondary secondary = new FusePoolLensSecondary();
     secondary.initialize(fusePoolDirectory);
 
-    deal(address(vars.bnb), accountOne, 5000000000e18);
-    deal(address(vars.bnb), accountThree, 5000000000e18);
+    deal(address(vars.mimo), accountOne, 5e27);
+    deal(address(vars.mimo), accountThree, 5e27);
     deal(address(vars.usdc), accountTwo, 10000e6);
 
     // Account One Supply
     {
       emit log("Account One Supply");
       vm.startPrank(accountOne);
-      vars.bnb.approve(address(cMimoToken), 1e36);
-      cMimoToken.mint(10000000e18);
+      vars.mimo.approve(address(cMimoToken), 1e36);
+      assertEq(cMimoToken.mint(10e24), 0, "!cmimo mint acc 1");
       vars.cTokens[0] = address(cMimoToken);
       comptroller.enterMarkets(vars.cTokens);
       vm.stopPrank();
@@ -182,8 +183,8 @@ contract MaxWithdrawTest is WithPool {
     {
       emit log("Account Three Supply");
       vm.startPrank(accountThree);
-      vars.bnb.approve(address(cMimoToken), 1e36);
-      cMimoToken.mint(10000000e18);
+      vars.mimo.approve(address(cMimoToken), 1e36);
+      assertEq(cMimoToken.mint(10e24), 0, "!cmimo mint acc 3");
       vars.cTokens[0] = address(cMimoToken);
       comptroller.enterMarkets(vars.cTokens);
       vm.stopPrank();
@@ -194,7 +195,7 @@ contract MaxWithdrawTest is WithPool {
       emit log("Account Two Supply");
       vm.startPrank(accountTwo);
       vars.usdc.approve(address(cUSDC), 1e36);
-      cUSDC.mint(1000e6);
+      assertEq(cUSDC.mint(1000e6), 0, "!cusdc mint acc 2");
       vars.cTokens[0] = address(cUSDC);
       comptroller.enterMarkets(vars.cTokens);
       vm.stopPrank();
@@ -206,11 +207,9 @@ contract MaxWithdrawTest is WithPool {
     {
       emit log("Account Two Borrow");
       vm.startPrank(accountTwo);
-      vars.bnb.approve(address(cMimoToken), 1e36);
-
-      uint256 maxBorrow = poolLensSecondary.getMaxBorrow(accountTwo, ICToken(address(cUSDC)));
+      uint256 maxBorrow = poolLensSecondary.getMaxBorrow(accountTwo, ICToken(address(cMimoToken)));
       emit log_uint(maxBorrow);
-      cMimoToken.borrow(maxBorrow);
+      assertEq(cMimoToken.borrow(maxBorrow), 0, "!cmimo borrow acc 2");
       assertEq(cMimoToken.totalBorrows(), maxBorrow, "!cMimo total borrows");
 
       vm.stopPrank();
@@ -221,14 +220,14 @@ contract MaxWithdrawTest is WithPool {
       emit log("Account One Borrow");
       vm.startPrank(accountOne);
       vars.usdc.approve(address(cUSDC), 1e36);
-      cUSDC.borrow(0.5e6);
-      assertEq(cUSDC.totalBorrows(), 0.5e6, "!cUSDC total borrows");
+      assertEq(cUSDC.borrow(150e6), 0, "!cusdc borrow acc 1");
+      assertEq(cUSDC.totalBorrows(), 150e6, "!cUSDC total borrows");
 
       uint256 maxWithdraw = poolLensSecondary.getMaxRedeem(accountOne, ICToken(address(cMimoToken)));
 
-      uint256 beforeMimoBalance = vars.bnb.balanceOf(accountOne);
+      uint256 beforeMimoBalance = vars.mimo.balanceOf(accountOne);
       cMimoToken.redeemUnderlying(type(uint256).max);
-      uint256 afterMimoBalance = vars.bnb.balanceOf(accountOne);
+      uint256 afterMimoBalance = vars.mimo.balanceOf(accountOne);
 
       assertEq(afterMimoBalance - beforeMimoBalance, maxWithdraw, "!mimo diff");
       vm.stopPrank();
