d5d054cf4bd56f69197d3330ed25609f24425b93
coolnft <haruto@midascapital.xyz>
2022-12-19 10:31:57 -0500

    MidasERC20wrapper added.

---
+++ contracts/midas/MidasERC20Wrapper.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import { ERC20Wrapper } from "openzeppelin-contracts/token/ERC20/extensions/ERC20Wrapper.sol";
+import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+contract MidasERC20Wrapper is ERC20Wrapper {
+
+  address private owner;
+
+  constructor(IERC20 underlyingToken) ERC20Wrapper(underlyingToken) {
+    owner = msg.sender;
+  }
+
+  function recover() public returns (uint256) {
+    return _recover(msg.sender);
+  }
+}
