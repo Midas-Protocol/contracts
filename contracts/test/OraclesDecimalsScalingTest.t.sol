16e11ab9af546c9bdfe3d0ae79438bfca0fe5feb
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-12-21 15:27:58 -0300

    upgrade fpd

--- contracts/test/OraclesDecimalsScalingTest.t.sol
+++ contracts/test/OraclesDecimalsScalingTest.t.sol
@@ -42,6 +42,8 @@ contract OraclesDecimalsScalingTest is BaseTest {
 
   function testOraclesDecimals() internal {
     if (address(fusePoolDirectory) != address(0)) {
+      // TODO: revert this after next deployment
+      upgradeFpd(address(fusePoolDirectory));
       (, FusePoolDirectory.FusePool[] memory pools) = fusePoolDirectory.getActivePools();
 
       for (uint8 i = 0; i < pools.length; i++) {
