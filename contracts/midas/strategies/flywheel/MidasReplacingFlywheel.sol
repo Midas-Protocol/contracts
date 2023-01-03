863de6b34feaae0630d40aed28e6382271193489
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-01-03 20:33:14 +0200

    replacing flywheel fixes

--- contracts/midas/strategies/flywheel/MidasReplacingFlywheel.sol
+++ contracts/midas/strategies/flywheel/MidasReplacingFlywheel.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.10;
 
 import { SafeOwnableUpgradeable } from "../../../midas/SafeOwnableUpgradeable.sol";
-import "./MidasFlywheelCore.sol";
+import "./MidasFlywheel.sol";
 
-contract MidasReplacingFlywheel is MidasFlywheelCore {
+contract MidasReplacingFlywheel is MidasFlywheel {
   MidasFlywheelCore public flywheelToReplace;
   mapping(address => bool) private rewardsTransferred;
 
@@ -20,27 +20,33 @@ contract MidasReplacingFlywheel is MidasFlywheelCore {
   }
 
   function rewardsAccrued(address user) public override returns (uint256) {
-    uint256 newStateRewardsAccrued = _rewardsAccrued[user];
-    if (newStateRewardsAccrued == 0 && !rewardsTransferred[user]) {
-      rewardsTransferred[user] = true;
-      _rewardsAccrued[user] = flywheelToReplace.rewardsAccrued(user);
+    if (address(flywheelToReplace) != address(0)) {
+      uint256 newStateRewardsAccrued = _rewardsAccrued[user];
+      if (newStateRewardsAccrued == 0 && !rewardsTransferred[user]) {
+        rewardsTransferred[user] = true;
+        _rewardsAccrued[user] = flywheelToReplace.rewardsAccrued(user);
+      }
     }
     return _rewardsAccrued[user];
   }
 
   function strategyState(ERC20 strategy) public override returns (uint224, uint32) {
-    RewardsState memory newStateStrategyState = _strategyState[strategy];
-    if (newStateStrategyState.index == 0) {
-      (uint224 index, uint32 ts) = flywheelToReplace.strategyState(strategy);
-      _strategyState[strategy] = RewardsState(index, ts);
+    if (address(flywheelToReplace) != address(0)) {
+      RewardsState memory newStateStrategyState = _strategyState[strategy];
+      if (newStateStrategyState.index == 0) {
+        (uint224 index, uint32 ts) = flywheelToReplace.strategyState(strategy);
+        _strategyState[strategy] = RewardsState(index, ts);
+      }
     }
     return (_strategyState[strategy].index, _strategyState[strategy].lastUpdatedTimestamp);
   }
 
   function userIndex(ERC20 strategy, address user) public override returns (uint224) {
-    uint224 newStateUserIndex = _userIndex[strategy][user];
-    if (newStateUserIndex == 0) {
-      _userIndex[strategy][user] = flywheelToReplace.userIndex(strategy, user);
+    if (address(flywheelToReplace) != address(0)) {
+      uint224 newStateUserIndex = _userIndex[strategy][user];
+      if (newStateUserIndex == 0) {
+        _userIndex[strategy][user] = flywheelToReplace.userIndex(strategy, user);
+      }
     }
     return _userIndex[strategy][user];
   }
