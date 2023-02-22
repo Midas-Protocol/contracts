c78538215d8a5d5caa0b0fa07260cae3d0079d8f
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-02-22 09:19:49 +0100

    autohedge fixes

--- contracts/test/autonomy/AutoHedgeLeveragedPositionTest.t.sol
+++ contracts/test/autonomy/AutoHedgeLeveragedPositionTest.t.sol
@@ -12,6 +12,7 @@ import "openzeppelin-contracts-upgradeable/contracts/security/ReentrancyGuardUpg
 
 import { Comptroller } from "../../compound/Comptroller.sol";
 import { ComptrollerInterface } from "../../compound/ComptrollerInterface.sol";
+import { CTokenInterface, CErc20Interface } from "../../compound/CTokenInterfaces.sol";
 import { InterestRateModel } from "../../compound/InterestRateModel.sol";
 import { IPriceOracle } from "../../external/compound/IPriceOracle.sol";
 import { MasterPriceOracle } from "../../oracles/MasterPriceOracle.sol";
@@ -19,12 +20,14 @@ import { IAutoHedgeLeveragedPosition } from "../../external/autonomy/IAutoHedgeL
 import { AutoHedgeOracle } from "../../oracles/default/AutoHedgeOracle.sol";
 import { IAutoHedgeLeveragedPositionFactory } from "../../external/autonomy/IAutoHedgeLeveragedPositionFactory.sol";
 import { IAutoHedgeStableVolatilePairUpgradeableV2 } from "../../external/autonomy/IAutoHedgeStableVolatilePairUpgradeableV2.sol";
+import { IAutoHedgeStableVolatileFactoryUpgradeableV2 } from "../../external/autonomy/IAutoHedgeStableVolatileFactoryUpgradeableV2.sol";
 import { IFlashloanWrapper } from "../../external/autonomy/IFlashloanWrapper.sol";
 
 import { BaseTest } from "../config/BaseTest.t.sol";
 
 contract AutoHedgeLeveragedPositionTest is BaseTest {
   Comptroller comptroller = Comptroller(0xEF0B026F93ba744cA3EDf799574538484c2C4f80);
+  CErc20Interface cAhlp;
   address POOL_ADMIN = 0x7dB8d33114462e032E5bf636D271f8680619Ba25;
   address FUSE_ADMIN = 0xFc1f56C58286E7215701A773b61bFf2e18A177dE;
   address busd = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
@@ -32,11 +35,13 @@ contract AutoHedgeLeveragedPositionTest is BaseTest {
   address JRM = 0x8A163f41414c005c3f551f2179bC9Ad8dcadE1C6;
   address CER20_IMPL_ADDRESS = 0x88d90463b4689aA9a622b711B0FA73c936E04B55;
   address FACTORY_PROXY = 0xa91799f00cA544ADb8bC4ea98B3406f59076E0Ee;
-  address WBNB_BUSD_PAIR = 0x457c94c9880fc5D576392310608355Cde22F9943;
+  IAutoHedgeStableVolatilePairUpgradeableV2 WBNB_BUSD_PAIR =
+    IAutoHedgeStableVolatilePairUpgradeableV2(0x457c94c9880fc5D576392310608355Cde22F9943);
   address PCS_FACTORY_ADDR = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
   address PCS_WBNB_BUSD_ADDR = 0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16;
   address SUSHI_BENTOBOX_ADDR = 0xF5BCE5077908a1b7370B9ae04AdC565EBd643966;
-  address PAIR_V1_BUSD_WBNB_ADDRESS = 0xd591c027F5415944E9183365cD6F192C6AcA4C71;
+  uint256 FLASH_LOAN_FEE = 50; // 0.05%
+  uint256 FLASH_LOAN_FEE_PRECISION = 1e5;
 
   function setUpBaseContracts() public {}
 
@@ -46,7 +51,7 @@ contract AutoHedgeLeveragedPositionTest is BaseTest {
     oracles[0] = IPriceOracle(address(ahOracle));
     MasterPriceOracle mpo = MasterPriceOracle(address(comptroller.oracle()));
     vm.prank(mpo.admin());
-    mpo.add(asArray(WBNB_BUSD_PAIR), oracles);
+    mpo.add(asArray(address(WBNB_BUSD_PAIR)), oracles);
   }
 
   function deployMarket() public fork(BSC_MAINNET) {
@@ -73,5 +78,19 @@ contract AutoHedgeLeveragedPositionTest is BaseTest {
   function testDeployAutoHedge() public fork(BSC_MAINNET) {
     deployOracle();
     deployMarket();
+    uint256 amountStableDepoist = 674186710914303100000;
+    uint256 amountStableFlashLoan = 2663354733161209274690;
+    uint256 levRatio = 5e18;
+    uint256 ahConvRate = 1e18 - IAutoHedgeStableVolatileFactoryUpgradeableV2(FACTORY_PROXY).depositFee();
+    uint256 b = (ahConvRate * (levRatio - 1e18)) / levRatio;
+    uint256 c = (1e18 * (FLASH_LOAN_FEE_PRECISION - FLASH_LOAN_FEE)) / FLASH_LOAN_FEE_PRECISION;
+
+    // deposit to pair
+    WBNB_BUSD_PAIR.deposit(amountStableDepoist, POOL_ADMIN, address(0));
+    vm.roll(1);
+    cAhlp = CErc20Interface(address(comptroller.cTokensByUnderlying(address(WBNB_BUSD_PAIR))));
+    IERC20(address(WBNB_BUSD_PAIR)).approve(address(cAhlp), 1e36);
+    uint256 ahlpAmount = IERC20(address(WBNB_BUSD_PAIR)).balanceOf(POOL_ADMIN);
+    cAhlp.mint(ahlpAmount);
   }
 }
