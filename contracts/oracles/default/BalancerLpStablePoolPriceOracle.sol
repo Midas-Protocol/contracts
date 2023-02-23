5b162ea27621432f1c1cdfb87a79ac4df5b70e4b
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-02-23 11:46:22 +0100

    balancer lp stable oracle

---
+++ contracts/oracles/default/BalancerLpStablePoolPriceOracle.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+
+import "../../external/compound/IPriceOracle.sol";
+import "../../external/compound/ICToken.sol";
+import "../../external/compound/ICErc20.sol";
+
+import { IBalancerStablePool } from "../../external/balancer/IBalancerStablePool.sol";
+import { IBalancerVault } from "../../external/balancer/IBalancerVault.sol";
+import { SafeOwnableUpgradeable } from "../../midas/SafeOwnableUpgradeable.sol";
+
+import { BasePriceOracle } from "../BasePriceOracle.sol";
+
+import { MasterPriceOracle } from "../MasterPriceOracle.sol";
+
+/**
+ * @title BalancerLpStablePoolPriceOracle
+ * @author Carlo Mazzaferro <carlo@midascapital.xyz> (https://github.com/carlomazzaferro)
+ * @notice BalancerLpStablePoolPriceOracle is a price oracle for Balancer LP tokens.
+ * @dev Implements the `PriceOracle` interface used by Midas pools (and Compound v2).
+ */
+contract BalancerLpStablePoolPriceOracle is SafeOwnableUpgradeable, BasePriceOracle {
+  mapping(address => address) public baseTokens;
+
+  address[] public lpTokens;
+
+  /**
+   * @dev Initializes an array of LP tokens and pools if desired.
+   * @param _lpTokens Array of LP token addresses.
+   * @param _baseTokens Array of base token addresses.
+   */
+  function initialize(address[] memory _lpTokens, address[] memory _baseTokens) public initializer {
+    require(_lpTokens.length == _baseTokens.length, "No LP tokens supplied or array lengths not equal.");
+    __SafeOwnable_init();
+
+    for (uint256 i = 0; i < _baseTokens.length; i++) {
+      baseTokens[_lpTokens[i]] = _baseTokens[i];
+    }
+  }
+
+  /**
+   * @notice Get the LP token price price for an underlying token address.
+   * @param underlying The underlying token address for which to get the price (set to zero address for ETH).
+   * @return Price denominated in ETH (scaled by 1e18).
+   */
+
+  function price(address underlying) external view override returns (uint256) {
+    return _price(underlying);
+  }
+
+  /**
+   * @notice Returns the price in ETH of the token underlying `cToken`.
+   * @dev Implements the `PriceOracle` interface for Fuse pools (and Compound v2).
+   * @return Price in ETH of the token underlying `cToken`, scaled by `10 ** (36 - underlyingDecimals)`.
+   */
+  function getUnderlyingPrice(ICToken cToken) external view override returns (uint256) {
+    address underlying = ICErc20(address(cToken)).underlying();
+    // Comptroller needs prices to be scaled by 1e(36 - decimals)
+    // Since `_price` returns prices scaled by 18 decimals, we must scale them by 1e(36 - 18 - decimals)
+    return (_price(underlying) * 1e18) / (10**uint256(ERC20Upgradeable(underlying).decimals()));
+  }
+
+  /**
+   * @dev Fetches the fair LP token/ETH price from Balancer, with 18 decimals of precision.
+   * Source: https://github.com/AlphaFinanceLab/homora-v2/blob/master/contracts/oracle/BalancerPairOracle.sol
+   */
+  function _price(address underlying) internal view virtual returns (uint256) {
+    IBalancerStablePool pool = IBalancerStablePool(underlying);
+
+    // Returns the BLP Token / Base Token rate
+    uint256 rate = pool.getRate();
+    uint256 baseTokenPrice = BasePriceOracle(msg.sender).price(baseTokens[underlying]);
+    return (rate * baseTokenPrice) / 1e18;
+  }
+}
---
+++ contracts/test/oracles/default/BalancerLpStablePoolPriceOracleTest.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import { MasterPriceOracle } from "../../../oracles/MasterPriceOracle.sol";
+import { IPriceOracle } from "../../../external/compound/IPriceOracle.sol";
+import { BalancerLpStablePoolPriceOracle } from "../../../oracles/default/BalancerLpStablePoolPriceOracle.sol";
+import { BaseTest } from "../../config/BaseTest.t.sol";
+
+contract BalancerLpStablePoolPriceOracleTest is BaseTest {
+  BalancerLpStablePoolPriceOracle oracle;
+  MasterPriceOracle mpo;
+
+  address stMATIC_WMATIC_pool = 0x8159462d255C1D24915CB51ec361F700174cD994;
+  address jBRL_BRZ_pool = 0xE22483774bd8611bE2Ad2F4194078DaC9159F4bA;
+  address boostedAavePool = 0x48e6B98ef6329f8f0A30eBB8c7C960330d648085;
+
+  address stMATIC = 0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4;
+  address jBRL = 0xf2f77FE7b8e66571E0fca7104c4d670BF1C8d722;
+  address usdt = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
+
+  function afterForkSetUp() internal override {
+    mpo = MasterPriceOracle(ap.getAddress("MasterPriceOracle"));
+    address[] memory lpTokens = asArray(stMATIC_WMATIC_pool, jBRL_BRZ_pool, boostedAavePool);
+
+    address[] memory baseTokens = asArray(stMATIC, jBRL, usdt);
+
+    oracle = new BalancerLpStablePoolPriceOracle();
+    oracle.initialize(lpTokens, baseTokens);
+  }
+
+  function getLpTokenPrice(address lpToken) internal returns (uint256) {
+    IPriceOracle[] memory oracles = new IPriceOracle[](1);
+    oracles[0] = IPriceOracle(oracle);
+
+    vm.prank(mpo.admin());
+    mpo.add(asArray(lpToken), oracles);
+    emit log("added the oracle");
+    return mpo.price(lpToken);
+  }
+
+  function testWmaticStmaticLpTokenOraclePrice() public fork(POLYGON_MAINNET) {
+    uint256 price = getLpTokenPrice(stMATIC_WMATIC_pool);
+    assertTrue(price > 0);
+    assertApproxEqAbs(price, mpo.price(stMATIC), 1e16);
+  }
+
+  function testJbrlBrzLpTokenOraclePrice() public fork(POLYGON_MAINNET) {
+    uint256 price = getLpTokenPrice(jBRL_BRZ_pool);
+    assertTrue(price > 0);
+    assertApproxEqAbs(price, mpo.price(jBRL), 1e16);
+  }
+
+  function testBoostedAaveLpTokenOraclePrice() public fork(POLYGON_MAINNET) {
+    uint256 price = getLpTokenPrice(boostedAavePool);
+    assertTrue(price > 0);
+    assertApproxEqAbs(price, mpo.price(usdt), 1e16);
+  }
+}
