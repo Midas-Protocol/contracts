c8b9dd358aed799664ed19d5958788440cfd64af
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-04-05 18:22:09 +0200

    license, sol version

--- contracts/utils/BalancerReentrancyAttacker.sol
+++ contracts/utils/BalancerReentrancyAttacker.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
 import { IBalancerStablePool } from "../external/balancer/IBalancerStablePool.sol";
 import "../external/balancer/IBalancerVault.sol";
 import { MasterPriceOracle } from "../oracles/MasterPriceOracle.sol";
