95b29c4dd4104ce61c25ea7471de0c65c4d605a2
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-04-05 18:19:56 +0200

    prettier

--- contracts/utils/BalancerReentrancyAttacker.sol
+++ contracts/utils/BalancerReentrancyAttacker.sol
@@ -3,37 +3,37 @@ import "../external/balancer/IBalancerVault.sol";
 import { MasterPriceOracle } from "../oracles/MasterPriceOracle.sol";
 
 contract BalancerReentrancyAttacker {
-    IBalancerVault private immutable _vault;
-    MasterPriceOracle private _mpo;
-    address private _lpToken;
+  IBalancerVault private immutable _vault;
+  MasterPriceOracle private _mpo;
+  address private _lpToken;
 
-    constructor(
-        IBalancerVault vault,
-        MasterPriceOracle mpo,
-        address lpToken
-    ) {
-        _vault = vault;
-        _mpo = mpo;
-        _lpToken = lpToken;
-    }
+  constructor(
+    IBalancerVault vault,
+    MasterPriceOracle mpo,
+    address lpToken
+  ) {
+    _vault = vault;
+    _mpo = mpo;
+    _lpToken = lpToken;
+  }
 
-    function startAttack() external payable {
-        UserBalanceOp[] memory ops = new UserBalanceOp[](1);
-        ops[0].kind = UserBalanceOpKind.DEPOSIT_INTERNAL;
-        // Asking to deposit 1 ETH
-        ops[0].amount = 1e18;
-        ops[0].sender = address(this);
-        ops[0].recipient = payable(address(this));
+  function startAttack() external payable {
+    UserBalanceOp[] memory ops = new UserBalanceOp[](1);
+    ops[0].kind = UserBalanceOpKind.DEPOSIT_INTERNAL;
+    // Asking to deposit 1 ETH
+    ops[0].amount = 1e18;
+    ops[0].sender = address(this);
+    ops[0].recipient = payable(address(this));
 
-        // but pass 2 eth, so there's an amount of exceding ETH and receive() callback is called
-        _vault.manageUserBalance{ value: 2e18 }(ops);
-    }
+    // but pass 2 eth, so there's an amount of exceding ETH and receive() callback is called
+    _vault.manageUserBalance{ value: 2e18 }(ops);
+  }
 
-    receive() external payable {
-        _reenterAttack();
-    }
+  receive() external payable {
+    _reenterAttack();
+  }
 
-    function _reenterAttack() internal view {
-        _mpo.price(_lpToken);
-    }
+  function _reenterAttack() internal view {
+    _mpo.price(_lpToken);
+  }
 }
