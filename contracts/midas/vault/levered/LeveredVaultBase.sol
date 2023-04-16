2969f692536d3c91b84da000236abd1b87e5c96b
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-04-16 10:52:51 +0300

    leverd vaults changes

--- contracts/midas/vault/levered/LeveredVaultBase.sol
+++ contracts/midas/vault/levered/LeveredVaultBase.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.10;
 
 import "../../DiamondExtension.sol";
 import { LeveredVaultStorage } from "./LeveredVaultStorage.sol";
+import "./LeveredVaultExtension.sol";
 
 contract LeveredVaultBase is LeveredVaultStorage, DiamondBase {
   /**
@@ -15,4 +16,14 @@ contract LeveredVaultBase is LeveredVaultStorage, DiamondBase {
     LibDiamond.registerExtension(extensionToAdd, extensionToReplace);
   }
 
+  function asExtension() public view returns (LeveredVaultExtension) {
+    return LeveredVaultExtension(address(this));
+  }
+
+  function initialize(LeveredVaultExtension[] calldata extensions, bytes calldata initData) public onlyOwner {
+    for (uint256 i; i < extensions.length; i++)
+      LibDiamond.registerExtension(extensions[i], DiamondExtension(address(0)));
+
+    asExtension().initialize(initData);
+  }
 }
