28d0d2b5a034b283f52d39f06882f9967df6857a
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-04-20 08:46:07 +0300

    test passing

--- contracts/test/ThenaLpERC4626Test.t.sol
+++ contracts/test/ThenaLpERC4626Test.t.sol
@@ -21,6 +21,9 @@ contract ThenaLpERC4626Test is BaseTest {
   function testThenaPluginDeposit() public fork(BSC_MAINNET) {
     address dpa = address(929292);
     ERC20 lpHayBusdToken = ERC20(0x93B32a8dfE10e9196403dd111974E325219aec24);
+    address lpTokenWhale = 0x29F82d09C2AfD12f3c10ee49CD713331f4A7228E;
+    vm.prank(lpTokenWhale);
+    lpHayBusdToken.transfer(address(this), 1e22);
 
     {
       ThenaLpERC4626 impl = new ThenaLpERC4626();
