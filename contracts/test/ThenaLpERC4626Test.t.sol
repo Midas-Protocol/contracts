ecc5118f76fb676fa82a0c4e918bc58ea4fdc95a
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-04-21 16:44:00 +0300

    testing the ankr rewards claiming

--- contracts/test/ThenaLpERC4626Test.t.sol
+++ contracts/test/ThenaLpERC4626Test.t.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0;
 import "./config/BaseTest.t.sol";
 import "../midas/strategies/ThenaLpERC4626.sol";
 import { CErc20PluginDelegate } from "../compound/CErc20PluginDelegate.sol";
+import { Comptroller } from "../compound/Comptroller.sol";
 
 import { ERC20Upgradeable as ERC20 } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
 import { ERC20 as SolERC20 } from "solmate/tokens/ERC20.sol";
@@ -61,7 +62,7 @@ contract ThenaLpERC4626Test is BaseTest {
     assertEq(sharesMinted, depositAmount, "!init shares");
   }
 
-  function testThenaPluginAccrueRewards() public fork(BSC_MAINNET) {
+  function testThenaPluginAccrueRewards() public debuggingOnly fork(BSC_MAINNET) {
     lpHayBusdToken.approve(address(plugin), 1e36);
     uint256 sharesMinted = plugin.deposit(1e16, address(this));
     emit log_named_uint("shares minted", sharesMinted);
@@ -83,18 +84,30 @@ contract ThenaLpERC4626Test is BaseTest {
   function testAnkrThenaRewards() public fork(BSC_MAINNET) {
     CErc20PluginDelegate market = CErc20PluginDelegate(marketAddress);
 
+    address user = 0x28C0208b7144B511C73586Bb07dE2100495e92f3;
+    Comptroller pool = Comptroller(address(market.comptroller()));
+    MidasFlywheel flywheel = MidasFlywheel(pool.rewardsDistributors(2));
+    flywheel.accrue(SolERC20(address(market)), user);
+
     plugin = ThenaLpERC4626(address(market.plugin()));
     ERC20 rewardToken = plugin.rewardTokens(0);
-    uint256 rewardsBalanceBefore = rewardToken.balanceOf(marketAddress);
+    assertEq(address(rewardToken), address(thenaToken), "not thena");
 
     vm.warp(block.timestamp + 1e7);
     vm.roll(block.number + 9999);
 
     plugin.claimRewards();
 
-    uint256 rewardsBalanceAfter = rewardToken.balanceOf(marketAddress);
+    uint256 rewardsBalanceBefore = rewardToken.balanceOf(user);
+
+    uint256 rewards = flywheel.accrue(SolERC20(address(market)), user);
+    assertGt(rewards, 0, "no rewards accrued");
+
+    flywheel.claimRewards(user);
+
+    uint256 rewardsBalanceAfter = rewardToken.balanceOf(user);
     uint256 rewardsDiff = rewardsBalanceAfter - rewardsBalanceBefore;
     emit log_named_uint("rewards diff", rewardsDiff);
-    assertGt(rewardsDiff, 0, "!no rewards claimed");
+    assertGt(rewardsDiff, 0, "!no rewards claimed for user");
   }
 }
