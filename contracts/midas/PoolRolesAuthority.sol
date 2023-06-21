a8cf0e18b35dc0bda5bc4ff0463c091adab87d9e
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-06-21 09:05:44 +0300

    compilation fixes

--- contracts/midas/PoolRolesAuthority.sol
+++ contracts/midas/PoolRolesAuthority.sol
@@ -2,12 +2,13 @@
 pragma solidity >=0.8.0;
 
 import { IComptroller } from "../compound/ComptrollerInterface.sol";
+import { ICErc20 } from "../compound/CTokenInterfaces.sol";
 
-import { RolesAuthority } from "solmate/auth/authorities/RolesAuthority.sol";
+import { RolesAuthority, Authority } from "solmate/auth/authorities/RolesAuthority.sol";
 
-import "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 
-contract PoolRolesAuthority is RolesAuthority, Initializeable {
+contract PoolRolesAuthority is RolesAuthority, Initializable {
   constructor() RolesAuthority(address(0), Authority(address(0))) {
     _disableInitializers();
   }
@@ -25,15 +26,15 @@ contract PoolRolesAuthority is RolesAuthority, Initializeable {
     ICErc20[] memory allMarkets = pool.getAllMarkets();
     for (uint256 i = 0; i < allMarkets.length; i++) {
       // TODO make fns gated
-      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), ICErc20.mint.selector, true);
-      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), ICErc20.redeem.selector, true);
-      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), ICErc20.redeemUnderlying.selector, true);
-      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), ICErc20.transfer.selector, true);
+      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), allMarkets[i].mint.selector, true);
+      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), allMarkets[i].redeem.selector, true);
+      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), allMarkets[i].redeemUnderlying.selector, true);
+      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), allMarkets[i].transfer.selector, true);
       // TODO fns needed at all?
-      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), ICErc20.transferFrom.selector, true);
-      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), ICErc20.approve.selector, true);
+      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), allMarkets[i].transferFrom.selector, true);
+      setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), allMarkets[i].approve.selector, true);
 
-      //setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), ICErc20.multicall.selector, true);
+      //setRoleCapability(SUPPLIER_ROLE, address(allMarkets[i]), allMarkets[i].multicall.selector, true);
     }
   }
 
@@ -42,10 +43,10 @@ contract PoolRolesAuthority is RolesAuthority, Initializeable {
     ICErc20[] memory allMarkets = pool.getAllMarkets();
     for (uint256 i = 0; i < allMarkets.length; i++) {
       // TODO make fns gated
-      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), ICErc20.borrow.selector, true);
-      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), ICErc20.repayBorrow.selector, true);
-      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), ICErc20.repayBorrowBehalf.selector, true);
-      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), ICErc20.flash.selector, true);
+      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), allMarkets[i].borrow.selector, true);
+      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), allMarkets[i].repayBorrow.selector, true);
+      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), allMarkets[i].repayBorrowBehalf.selector, true);
+      setRoleCapability(BORROWER_ROLE, address(allMarkets[i]), allMarkets[i].flash.selector, true);
     }
   }
 
@@ -53,9 +54,9 @@ contract PoolRolesAuthority is RolesAuthority, Initializeable {
     ICErc20[] memory allMarkets = pool.getAllMarkets();
     for (uint256 i = 0; i < allMarkets.length; i++) {
       // TODO make fns gated
-      setRoleCapability(LIQUIDATOR_ROLE, address(allMarkets[i]), ICErc20.liquidateBorrow.selector, true);
+      setRoleCapability(LIQUIDATOR_ROLE, address(allMarkets[i]), allMarkets[i].liquidateBorrow.selector, true);
       // seize is called by other CTokens
-      //setRoleCapability(LIQUIDATOR_ROLE, address(allMarkets[i]), ICErc20.seize.selector, true);
+      //setRoleCapability(LIQUIDATOR_ROLE, address(allMarkets[i]), allMarkets[i].seize.selector, true);
     }
   }
 }
